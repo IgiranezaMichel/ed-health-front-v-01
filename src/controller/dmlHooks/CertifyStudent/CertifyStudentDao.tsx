eb83c17ec1179d8f735812a5db7eb8c2aaa7ff76
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2022-03-08 19:05:37 +0200

    add saveCertifyStudent mutation

--- src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
+++ src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
@@ -1,3 +1,6 @@
-export const useSaveCertifyStudent=(trainingApplicationId:number,applicationStatus:string,certifyStudentInput:CertifiedStudentInput)=>{
+import { useMutation } from "@apollo/client"
+import { CERTIFY_STUDENT } from "../../../graphQl/mutation/CertifiedStudentMutations"
 
+export const useSaveCertifyStudent=(trainingApplicationId:number,applicationStatus:string,certifyStudentInput:CertifiedStudentInput)=>{
+const [saveCertifyStudent]=useMutation(CERTIFY_STUDENT);
 }
