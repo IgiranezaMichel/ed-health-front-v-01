337a7cad915d4d9f8546ebd188b8fc0e04ad3a10
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2022-03-08 19:00:37 +0200

    add mutation

--- src/graphQl/mutation/CertifiedStudentMutations.tsx
+++ src/graphQl/mutation/CertifiedStudentMutations.tsx
@@ -1,5 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const CERTIFY_STUDENT=gql`
-
+mutation(trainingApplicationId:Long,applicationStatus:String,certifyStudentInput:CertifiedStudentInput){
+    certifyStudent(trainingApplicationId:$trainingApplicationId,applicationStatus:$applicationStatus,certifyStudentInput:$certifyStudentInput)
+}
 `
