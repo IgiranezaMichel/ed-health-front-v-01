739d203f6ff37e0b960152586456d4b18c7a6c0e
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2022-03-08 19:08:30 +0200

    add response data

--- src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
+++ src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
@@ -1,9 +1,12 @@
 import { useMutation } from "@apollo/client"
 import { CERTIFY_STUDENT } from "../../../graphQl/mutation/CertifiedStudentMutations"
+import { useState } from "react";
 
 export const useSaveCertifyStudent=(trainingApplicationId:number,applicationStatus:string,certifyStudentInput:CertifiedStudentInput)=>{
 const [saveCertifyStudent]=useMutation(CERTIFY_STUDENT);
+const [message,setMessage]=useState('');
 const registerHandler=()=>{
     saveCertifyStudent({variables:{trainingApplicationId:trainingApplicationId,applicationStatus:applicationStatus,certifyStudentInput:certifyStudentInput}})
+    .then(data=>setMessage(data.data.certifyStudent));
 }
 }
