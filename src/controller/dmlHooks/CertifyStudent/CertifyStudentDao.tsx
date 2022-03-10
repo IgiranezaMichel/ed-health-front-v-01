1305ff6e6ee7ad4d485417d582f591fb12cf43e1
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2022-03-10 08:30:15 +0200

    import CertifiedStudentInput

--- src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
+++ src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from "@apollo/client"
 import { CERTIFY_STUDENT } from "../../../graphQl/mutation/CertifiedStudentMutations"
 import { useState } from "react";
+import { CertifiedStudentInput } from "../../../typeDefs/CertifiedStudentInput";
 
 export const useSaveCertifyStudent=(trainingApplicationId:number,applicationStatus:string,certifyStudentInput:CertifiedStudentInput)=>{
 const [saveCertifyStudent]=useMutation(CERTIFY_STUDENT);
