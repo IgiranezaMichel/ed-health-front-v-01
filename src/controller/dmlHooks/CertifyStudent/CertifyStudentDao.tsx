bdd847dd68b92b54a713973b6b21bf0d4610cf35
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2022-03-11 09:11:25 +0200

    add isLoading

--- src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
+++ src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
@@ -7,9 +7,10 @@ import { CertificateStatus } from "../../../enums/CertificateStatus";
 export const useSaveCertifyStudent=(trainingApplicationId:number,applicationStatus:CertificateStatus,certifyStudentInput:CertifiedStudentInput)=>{
 const [saveCertifyStudent]=useMutation(CERTIFY_STUDENT);
 const [message,setMessage]=useState('');
+const [isLoading,setIsLoading]=useState(true);
 const registerHandler=()=>{
     saveCertifyStudent({variables:{trainingApplicationId:trainingApplicationId,applicationStatus:applicationStatus,certifyStudentInput:certifyStudentInput}})
-    .then(data=>setMessage(data.data.certifyStudent))
+    .then(data=>{setMessage(data.data.certifyStudent);setIsLoading(false)})
     .catch(err=>err)
 }
 return {message,registerHandler}
