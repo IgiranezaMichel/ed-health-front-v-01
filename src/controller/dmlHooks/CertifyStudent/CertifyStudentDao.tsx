08c60a3ce6177b8b2d10af1f14f13d68d905f430
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2022-03-11 09:16:59 +0200

    remove un used import

--- src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
+++ src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
@@ -2,7 +2,6 @@ import { useMutation } from "@apollo/client"
 import { CERTIFY_STUDENT } from "../../../graphQl/mutation/CertifiedStudentMutations"
 import { useState } from "react";
 import { CertifiedStudentInput } from "../../../typeDefs/CertifiedStudentInput";
-import { CertificateStatus } from "../../../enums/CertificateStatus";
 
 export const useSaveCertifyStudent=(trainingApplicationId:number,applicationStatus:string,certifyStudentInput:CertifiedStudentInput)=>{
 const [saveCertifyStudent]=useMutation(CERTIFY_STUDENT);
