885c66c1d8ae6e177ad2c863878b5c5590ee64e7
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2022-03-11 09:14:46 +0200

    remove status

--- src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
+++ src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
@@ -2,14 +2,13 @@ import { useMutation } from "@apollo/client"
 import { CERTIFY_STUDENT } from "../../../graphQl/mutation/CertifiedStudentMutations"
 import { useState } from "react";
 import { CertifiedStudentInput } from "../../../typeDefs/CertifiedStudentInput";
-import { CertificateStatus } from "../../../enums/CertificateStatus";
 
-export const useSaveCertifyStudent=(trainingApplicationId:number,applicationStatus:CertificateStatus,certifyStudentInput:CertifiedStudentInput)=>{
+export const useSaveCertifyStudent=(trainingApplicationId:number,certifyStudentInput:CertifiedStudentInput)=>{
 const [saveCertifyStudent]=useMutation(CERTIFY_STUDENT);
 const [message,setMessage]=useState('');
 const [isLoading,setIsLoading]=useState(true);
 const registerHandler=()=>{
-    saveCertifyStudent({variables:{trainingApplicationId:trainingApplicationId,applicationStatus:applicationStatus,certifyStudentInput:certifyStudentInput}})
+    saveCertifyStudent({variables:{trainingApplicationId:trainingApplicationId,certifyStudentInput:certifyStudentInput}})
     .then(data=>{setMessage(data.data.certifyStudent);setIsLoading(false)})
     .catch(err=>err)
 }
