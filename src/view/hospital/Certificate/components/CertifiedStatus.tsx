611d8b646133942b377acbd8494f5ebbdf48c544
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2022-03-14 09:24:16 +0200

    add studentApplicatonStatus

--- src/view/hospital/Certificate/components/CertifiedStatus.tsx
+++ src/view/hospital/Certificate/components/CertifiedStatus.tsx
@@ -11,10 +11,10 @@ import { CertificateStatus } from "../../../../enums/CertificateStatus";
 
 export const CertifiedStatus = (props: { status: string, trainingId: number }) => {
     const [page, setPage] = useState<PaginationInput>({ pageNumber: 0, pageSize: 10, sort: "id" });
-    const [status, setStatus] = useState('approved');
+    const [studentApplicatonStatus, setStudentApplicationStatus] = useState('approved');
     const certifiedStudents = useGetCertifiedStudentByAdminApprovalStatus(props.status, props.trainingId, page);
     const [certifyStudent,setCertifyStudent]=useState<CertifiedStudentInput>(
-        {certificateId:0,CertificateStatus:status,id:0,studentId:0}
+        {certificateId:0,CertificateStatus:,id:0,studentId:0}
         );
     const saveCertifyStudent=useSaveCertifyStudent(props.trainingId,status,certifyStudent);
     const registerCertifyStudent=()=>{
