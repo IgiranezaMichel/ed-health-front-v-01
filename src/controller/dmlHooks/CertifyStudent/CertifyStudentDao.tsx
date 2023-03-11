4c1d20c733f256fa8c8a7654d9b93f76c24dcec1
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-03-11 11:46:39 +0200

    add logs

--- src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
+++ src/controller/dmlHooks/CertifyStudent/CertifyStudentDao.tsx
@@ -4,16 +4,15 @@ import { useState } from "react";
 import { CertifiedStudentInput } from "../../../typeDefs/CertifiedStudentInput";
 
 export const useSaveCertifyStudent=(trainingApplicationId:number,applicationStatus:string,certifyStudentInput:CertifiedStudentInput)=>{
-    console.log(trainingApplicationId)
-    console.log(applicationStatus)
-    console.log(certifyStudentInput)
 const [saveCertifyStudent]=useMutation(CERTIFY_STUDENT);
 const [message,setMessage]=useState('');
 const [isLoading,setIsLoading]=useState(true);
 const registerHandler=()=>{
     saveCertifyStudent({variables:{trainingApplicationId:trainingApplicationId,applicationStatus:applicationStatus,certifyStudentInput:certifyStudentInput}})
-    .then(data=>{setMessage(data.data.certifyStudent);setIsLoading(false)})
-    .catch(err=>err)
+    .then(data=>{setMessage(data.data.certifyStudent);
+        console.log(data.data)
+        setIsLoading(false)})
+    .catch(err=>console.log(err))
 }
 return {message,registerHandler,isLoading}
 }
