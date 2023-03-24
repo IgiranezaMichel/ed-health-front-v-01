86a28d2e00f2fa1315e9ebd09a4aabf72a7bb288
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-03-24 16:03:27 +0200

    format

--- src/App.tsx
+++ src/App.tsx
@@ -34,48 +34,48 @@ import { StudentCertificate } from './view/student/certificate'
 import { TrainingCertificate } from './view/hospital/Certificate/index'
 function App() {
   return (
-   <>
-    <Router>
-    <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/student' element={<Student/>}/>
-      <Route path='/student/training' element={<StudentTraining/>}/>
-      <Route path='/student/job' element={<JobPublished/>}/>
-      <Route path='/student/certificate' element={<StudentCertificate/>}/>
-      <Route path='/:name/setting' element={<Setting/>}/>
-       <Route path='/admin' element={<AdminHome/>}/>  
-      <Route path='/admin/users' element={<UserManagement/>}/>
-      <Route path='/admin/school' element={<SchoolManagement/>}/>
-      <Route path='/:user/school-detail/:id' element={<SchoolDetail/>}/>
-      <Route path='/admin/hospital' element={<HospitalManagement/>}/> 
- {/* ncnm  */}
-      <Route path='/ncnm' element={<NcnmDashboard/>}/>
-      <Route path='/ncnm/school' element={<NcnmSchool/>}/>
-      <Route path='/ncnm/hospital' element={<NcnmHospital/>}/>
-      <Route path='/ncnm/training' element={<NcnmTraining/>}/>
-      <Route path='/ncnm/training-detail/:trainingId' element={<TrainingDetail/>}/>
-      <Route path='/ncnm/certificate' element={<NcnmCertificate/>}/>
-      <Route path='/ncnm/payment' element={<NcnmPayment/>}/>
-      <Route path='/ncnm/hospital-detail/:id' element={<HospitalDetail/>}/>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/student' element={<Student />} />
+          <Route path='/student/training' element={<StudentTraining />} />
+          <Route path='/student/job' element={<JobPublished />} />
+          <Route path='/student/certificate' element={<StudentCertificate />} />
+          <Route path='/:name/setting' element={<Setting />} />
+          <Route path='/admin' element={<AdminHome />} />
+          <Route path='/admin/users' element={<UserManagement />} />
+          <Route path='/admin/school' element={<SchoolManagement />} />
+          <Route path='/:user/school-detail/:id' element={<SchoolDetail />} />
+          <Route path='/admin/hospital' element={<HospitalManagement />} />
+          {/* ncnm  */}
+          <Route path='/ncnm' element={<NcnmDashboard />} />
+          <Route path='/ncnm/school' element={<NcnmSchool />} />
+          <Route path='/ncnm/hospital' element={<NcnmHospital />} />
+          <Route path='/ncnm/training' element={<NcnmTraining />} />
+          <Route path='/ncnm/training-detail/:trainingId' element={<TrainingDetail />} />
+          <Route path='/ncnm/certificate' element={<NcnmCertificate />} />
+          <Route path='/ncnm/payment' element={<NcnmPayment />} />
+          <Route path='/ncnm/hospital-detail/:id' element={<HospitalDetail />} />
 
-      {/* school */}
-      <Route path='/school' element={<SchoolDashBoard/>}/>
-      <Route path='/school/student' element={<SchoolStudents/>}/>
-      <Route path='/school/activity' element={<SchoolActivity/>}/>
-      <Route path='/hospital' element={<HospitalDashboard/>}/>
-      <Route path='/hospital/training' element={<HospitalTraining/>}/>
-      <Route path='/hospital/training-detail/:id' element={<HospitalTrainingDetail/>}/>
-      <Route path='/hospital/job' element={<Job/>}/>
-      <Route path='/hospital/training-applicant-detail/:trainingId' element={<TrainingApplicationDetail/>}/>
-      <Route path='/hospital/:trainingId' element={<TrainingApplicant/>}/>
-      <Route path='/hospital/job-detail/:id' element={<JobDetail/>}/>
-      <Route path='/hospital/notification' element={<HospitalNotification/>}/>
-      <Route path='/hospital/certificate' element={<Certificate/>}/>
-      <Route path='/hospital/certificate/:trainingId' element={<TrainingCertificate/>}/>
-      <Route path='/:name/logout' element={<Logout/>}/>
-    </Routes>
- </Router>
-   </>
+          {/* school */}
+          <Route path='/school' element={<SchoolDashBoard />} />
+          <Route path='/school/student' element={<SchoolStudents />} />
+          <Route path='/school/activity' element={<SchoolActivity />} />
+          <Route path='/hospital' element={<HospitalDashboard />} />
+          <Route path='/hospital/training' element={<HospitalTraining />} />
+          <Route path='/hospital/training-detail/:id' element={<HospitalTrainingDetail />} />
+          <Route path='/hospital/job' element={<Job />} />
+          <Route path='/hospital/training-applicant-detail/:trainingId' element={<TrainingApplicationDetail />} />
+          <Route path='/hospital/:trainingId' element={<TrainingApplicant />} />
+          <Route path='/hospital/job-detail/:id' element={<JobDetail />} />
+          <Route path='/hospital/notification' element={<HospitalNotification />} />
+          <Route path='/hospital/certificate' element={<Certificate />} />
+          <Route path='/hospital/certificate/:trainingId' element={<TrainingCertificate />} />
+          <Route path='/:name/logout' element={<Logout />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 export default App
