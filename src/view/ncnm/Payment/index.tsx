f73e7f68a0a3f47cc813f91be8653c38d00ef5a6
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-03-29 16:18:32 +0200

    import payment

--- src/view/ncnm/Payment/index.tsx
+++ src/view/ncnm/Payment/index.tsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material"
 import { NcnmMenu } from "../../../MenuBarItems/NcnmMenu"
 import { Navigation } from "../../../components/default/Navigation"
 import { ListAltRounded } from "@mui/icons-material"
+import { Payment } from "./components/payment"
 
  export const NcnmPayment=()=>{
     return<Navigation items={NcnmMenu}>
@@ -9,7 +10,7 @@ import { ListAltRounded } from "@mui/icons-material"
       <div>
          <Button variant="contained" className="fw-bolder m-1 rounded-0"><ListAltRounded/>Recent payment certificate</Button>
          <Button variant="contained" className="text-white fw-bolder m-1 rounded-0"><ListAltRounded/>Unpaid certificate</Button>
-         
+         <Payment/>
       </div>
     </Navigation>
  }
