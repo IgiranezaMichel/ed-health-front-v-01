99a41c3b01037b6ec55882eb5730af8716ece39f
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-04-02 15:46:27 +0200

    add person icon

--- src/view/ncnm/Payment/components/payment.tsx
+++ src/view/ncnm/Payment/components/payment.tsx
@@ -1,11 +1,17 @@
+import { Person } from "@mui/icons-material"
 import { Card } from "@mui/material"
 
 export const Payment=()=>{
     return(
         <>
         <span className="d-block display-6 fw-bolder mt-4">Recent certificate</span>
-       <Card elevation={5} className="mb-3">
-        
+       <Card elevation={5} className="mb-3 row col-sm-12 m-auto">
+        <div className="col-md-2">
+            User img
+        </div>
+        <div className="col-md-4">
+            <div><Person/></div>
+        </div>
        </Card>
         </>
     )
