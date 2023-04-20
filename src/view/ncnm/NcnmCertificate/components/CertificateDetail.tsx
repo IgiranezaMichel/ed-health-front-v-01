899fed892966ed620acc1f2c0b469a71b80908bc
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-04-20 19:56:39 +0200

    add training state

--- src/view/ncnm/NcnmCertificate/components/CertificateDetail.tsx
+++ src/view/ncnm/NcnmCertificate/components/CertificateDetail.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { CalendarMonthOutlined, ListAltOutlined, Person2, Phone } from "@mui/icons-material";
-import { Card, CircularProgress } from "@mui/material";
+import { CalendarMonthOutlined, ListAltOutlined, Person2, Phone, Visibility, VisibilityOff } from "@mui/icons-material";
+import { Button, Card, CircularProgress } from "@mui/material";
 import { CalendarIcon } from "@mui/x-date-pickers";
 import useFindCertificateById from "../../../../controller/viewHooks/useFindCertificateById";
 import { PIE_CHART_DEFAULT } from "../../../../components/default/PIECHART";
+import { useState } from "react";
 
 export const CertificateDetail = (props: { certificateId: number }) => {
     const { certificateDetail, isLoadingCertificate } = useFindCertificateById(props.certificateId);
+    const [showTrainer,setShowTrainer]=useState(false);
     console.log(certificateDetail)
     console.log(isLoadingCertificate);
     return (
@@ -15,24 +17,45 @@ export const CertificateDetail = (props: { certificateId: number }) => {
                 <CircularProgress />
             </div>}
             {!isLoadingCertificate && <Card elevation={5} className="col-12 m-auto row">
-                <Card className="mt-4 p-0 col-md-7 rounded-0">
+                <Card className="mt-4 p-0 col-md-6 rounded-0">
                     <PIE_CHART_DEFAULT items={[{label:'',value:10}]} centerLabel="Certified status"/>
                 </Card>
-                <Card className="mt-4 col-md-5 rounded-0">
-                    <img width={50} height={50} className="rounded-circle" src={certificateDetail.training.hospital.logo} alt={certificateDetail.training.hospital.name} />
-                    <div className="mb-1"><b style={{fontFamily:'monospace'}}>Hospital </b>{certificateDetail.training.hospital.name}</div>
+                <Card className="mt-4 col-md-6 rounded-0 d-flex align-items-center">
+                   <section className="">
+                   <div className="mb-1"><b style={{fontFamily:'sans-serif'}}> Hospital </b>{certificateDetail.training.hospital.name}</div>
                     <div>{certificateDetail.training.description}</div>
-                    <div className="mb-1"><b>Training Title</b>{certificateDetail.training.title}</div>
-                    <div className="mb-1"><b>Total Participant </b><span
+                    <div className="mb-1"><b style={{fontFamily:'sans-serif'}}> Training Title</b>{certificateDetail.training.title}</div>
+                    <div className="mb-1"><b style={{fontFamily:'sans-serif'}}>Total Participant </b><span
                         className="badge bg-primary">New!</span>
                     </div>
-                    <div className=""><b>Training Location </b>
+                    <div className=""><b style={{fontFamily:'sans-serif'}}>Training Location </b>
                     {certificateDetail.training.location.Location.Location.name}/{certificateDetail.training.location.Location.name}/
                     {certificateDetail.training.location.name}
                     </div>
                     <div>{certificateDetail.training.description}</div>
                     <div><CalendarMonthOutlined />{String(certificateDetail.training.deadline).split('T')[0]}</div>
                     <span className="float-end">Published at {String(certificateDetail.training.timeStamp).split('T')[0]}</span>
+                   </section>
+                </Card>
+                <Card className="col-sm-12 mt-2 p-2" elevation={9}>
+                    <span className="fw-bolder p-2 display-4">Trainer</span>
+                    <Button onClick={()=>setShowTrainer(!showTrainer)} className="float-end">{showTrainer?<Visibility/>:<VisibilityOff/>}</Button>
+                   {showTrainer&&<section className="d-flex justify-content-center align-content-center">
+                   <div className="row container-lg m-auto g-2">
+                        {certificateDetail.training.trainers.map((data:any,index:any)=>{
+                            return  <div key={index} className="col-sm-2 mt-3">
+                                <Card className="p-1 rounded-0" elevation={4}>
+                                <div className="text-center p-2">
+                                <img height={100} src={data.profilePicture} alt="" />
+                                </div>
+                               <div><Person2/><b>{data.trainerTitle}</b> <span>{data.name}</span></div> 
+                               <div><Phone/> {data.phoneNumber}</div> 
+                               </Card>
+                            </div>
+                          
+                        })}
+                    </div>
+                   </section>}
                 </Card>
                 <Card className="col-sm-6 mt-2 rounded-0" elevation={9}>
                     <div className="fw-bold p-3 fs-4">Certificate</div>
@@ -45,22 +68,9 @@ export const CertificateDetail = (props: { certificateId: number }) => {
                    <div className="mt-4 ">
                     <ListAltOutlined/>
                    </div>
+                Certificate
                 </Card>
-                <Card className="col-sm-6 mt-2" elevation={9}>
-                    <span className="fw-bolder p-2">Trainer</span>
-                    <div className="row col-12 m-auto g-2">
-                        {certificateDetail.training.trainers.map((data:any,index:any)=>{
-                            return  <div key={index} className="col-sm-6 mt-3">
-                                <Card >
-                                <img className="card-img" src={data.profilePicture} alt="" />
-                               <div><Person2/><b>{data.trainerTitle}</b> <span>{data.name}</span></div> 
-                               <div><Phone/> {data.phoneNumber}</div> 
-                               </Card>
-                            </div>
-                          
-                        })}
-                    </div>
-                </Card>
+                
             </Card>}
         </>
     )
