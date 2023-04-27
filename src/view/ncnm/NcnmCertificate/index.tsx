c69ead6bb3a77c4ac35cd3ab1fc6385a0c54d8ad
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-04-27 20:51:50 +0200

    remove unwanted spaces

--- src/view/ncnm/NcnmCertificate/index.tsx
+++ src/view/ncnm/NcnmCertificate/index.tsx
@@ -1,22 +1,20 @@
-import { Button} from "@mui/material"
+import { Button } from "@mui/material"
 import { NcnmMenu } from "../../../MenuBarItems/NcnmMenu"
 import { Navigation } from "../../../components/default/Navigation"
 import { List, PeopleAltOutlined } from "@mui/icons-material"
 import { Certificate } from "./components/Certificate"
 
- 
-export const NcnmCertificate=()=>{
-    return(
+export const NcnmCertificate = () => {
+    return (
         <Navigation items={NcnmMenu}>
             <span className="display-6 fw-bold">Certificate</span>
             <div>
-                <Button className="bg-info text-light fw-bold rounded-0 m-1"><List className="text-light"/>Certificate</Button>
-                <Button className="bg-info text-light fw-bold rounded-0 m-1"><PeopleAltOutlined/>Certified Users</Button>
-            
+                <Button className="bg-info text-light fw-bold rounded-0 m-1"><List className="text-light" />Certificate</Button>
+                <Button className="bg-info text-light fw-bold rounded-0 m-1"><PeopleAltOutlined />Certified Users</Button>
             </div>
-        <div className="m-auto">
-        <Certificate/>
-        </div>
+            <div className="m-auto">
+                <Certificate />
+            </div>
         </Navigation>
     )
 }
