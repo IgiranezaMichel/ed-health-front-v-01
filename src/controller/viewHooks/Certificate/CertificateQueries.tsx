45f85ee6e9741da813aa33875d5c7afafb19ae8c
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-05 16:43:34 +0200

    create use effect

---
+++ src/controller/viewHooks/Certificate/CertificateQueries.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useQuery } from "@apollo/client"
+import { _FIND_CERTIFICATE_BY_TRAINING_ID } from "../../../graphQl/queries/CertificateQueries"
+import { useEffect, useState } from "react";
+
+export const useFindCertificateByTrainingId=(trainingId:number)=>{
+const {data,refetch}=useQuery(_FIND_CERTIFICATE_BY_TRAINING_ID,{variables:{trainingId:trainingId}});
+const [certificateList,setCertificateList]=useState<any>([]);
+useEffect(
+    ()=>{
+        
+    }
+)
+
+}
