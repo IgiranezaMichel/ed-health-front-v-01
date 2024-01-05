5ab3765779aff3c02b98c02a409387429273d313
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-05 17:04:21 +0200

    import find certificate by training id

--- src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
+++ src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
@@ -1,6 +1,9 @@
 import { Card } from "@mui/material"
+import { useFindCertificateByTrainingId_ } from "../../../../controller/viewHooks/Certificate/CertificateQueries";
 
 export const RegisteredCertificateList = (props: { trainingId: number }) => {
+    const certificateList=useFindCertificateByTrainingId_(props.trainingId);
+    console.log(certificateList.certificateList)
     return <>
         <Card elevation={4} className="border">
             <div className="fw-bold p-4 text-center">
