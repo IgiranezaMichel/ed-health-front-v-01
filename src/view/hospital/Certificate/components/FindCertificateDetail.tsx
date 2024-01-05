6cab3512720abcf9924ea33092138467186f66fa
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-05 16:56:04 +0200

    create parameter

--- src/view/hospital/Certificate/components/FindCertificateDetail.tsx
+++ src/view/hospital/Certificate/components/FindCertificateDetail.tsx
@@ -1,6 +1,6 @@
 import { useFindCertificateByTrainingId_ } from "../../../../controller/viewHooks/Certificate/CertificateQueries"
 
-export const FindCertificateDetail=()=>{
+export const FindCertificateDetail=(props:{trainingId:number})=>{
     useFindCertificateByTrainingId_()
     return(
         <>
