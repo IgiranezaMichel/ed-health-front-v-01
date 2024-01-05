7e68137af204c092fc69dcd2cce14905e54e6948
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-05 16:57:54 +0200

    change trainingdatas  variable to certificateList

--- src/view/hospital/Certificate/components/FindCertificateDetail.tsx
+++ src/view/hospital/Certificate/components/FindCertificateDetail.tsx
@@ -1,7 +1,7 @@
 import { useFindCertificateByTrainingId_ } from "../../../../controller/viewHooks/Certificate/CertificateQueries"
 
 export const FindCertificateDetail=(props:{trainingId:number})=>{
-    const trainingData=useFindCertificateByTrainingId_(props.trainingId);
+    const certificateList=useFindCertificateByTrainingId_(props.trainingId);
     return(
         <>
         </>
