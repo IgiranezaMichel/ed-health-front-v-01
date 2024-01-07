1e50d53a61209ae3e914f9edc7d3380c5e4744b0
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-07 17:24:28 +0200

    remove un necesssary import

--- src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
+++ src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
@@ -1,7 +1,5 @@
 import { Card, CircularProgress } from "@mui/material"
 import { useFindCertificateByTrainingId_ } from "../../../../controller/viewHooks/Certificate/CertificateQueries";
-import { Certificate } from "../../Certificate";
-
 export const RegisteredCertificateList = (props: { trainingId: number }) => {
     const certificateList=useFindCertificateByTrainingId_(props.trainingId);
     console.log(certificateList.certificateList)
