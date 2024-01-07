3a5c6ec1eb0bb9f64c3926d9e469e282b699772f
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-07 17:31:44 +0200

    add data of certificate

--- src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
+++ src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
@@ -1,15 +1,24 @@
-import { Card, CircularProgress } from "@mui/material"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Card, CircularProgress, Divider } from "@mui/material"
 import { useFindCertificateByTrainingId_ } from "../../../../controller/viewHooks/Certificate/CertificateQueries";
 export const RegisteredCertificateList = (props: { trainingId: number }) => {
     const certificateList=useFindCertificateByTrainingId_(props.trainingId);
     console.log(certificateList.certificateList)
     return <>
-        <Card elevation={4} className="border">
+        <>
             {certificateList.isLoading?<div className="fw-bold p-4 text-center">
                 <CircularProgress/>
             </div>:<>
-            --No Certificate found --
+            {
+                certificateList.certificateList.map((data:any)=>{
+                    return <Card elevation={4} className="border m-2">
+                       <div>Certificate {data.title}</div>
+                       <Divider/>
+                       has been approved by {data.accountHolder.name}
+                    </Card>
+                })
+            }
             </>}
-        </Card>
+        </>
     </>
 }
