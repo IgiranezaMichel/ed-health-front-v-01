cc678098d0d56c2a8a9e0f2ff0731ab99507e38a
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-07 17:45:40 +0200

    format document

--- src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
+++ src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
@@ -5,25 +5,25 @@ import { RemoveRedEyeRounded } from "@mui/icons-material";
 import QrCode from "../../../../components/default/QrCode";
 import { Qrcode } from "../../../../qrcode";
 export const RegisteredCertificateList = (props: { trainingId: number }) => {
-    const certificateList=useFindCertificateByTrainingId_(props.trainingId);
+    const certificateList = useFindCertificateByTrainingId_(props.trainingId);
     console.log(certificateList.certificateList)
     return <>
         <>
-            {certificateList.isLoading?<div className="fw-bold p-4 text-center">
-                <CircularProgress/>
-            </div>:<>
-            {
-                certificateList.certificateList.map((data:any)=>{
-                    return <Card elevation={4} className="border m-2 p-2">
-                       <div className="fs-4 fw-bold" style={{fontFamily:'cursive'}}>Certificate {data.title}</div>
-                       <Divider/>
-                       has been approved by {data.accountHolder.name}
-                       <div className="modal-footer">
-                        <RemoveRedEyeRounded/>
-                       </div>
-                    </Card>
-                })
-            }
+            {certificateList.isLoading ? <div className="fw-bold p-4 text-center">
+                <CircularProgress />
+            </div> : <>
+                {
+                    certificateList.certificateList.map((data: any) => {
+                        return <Card elevation={4} className="border m-2 p-2">
+                            <div className="fs-4 fw-bold" style={{ fontFamily: 'cursive' }}>Certificate {data.title}</div>
+                            <Divider />
+                            has been approved by {data.accountHolder.name}
+                            <div className="modal-footer">
+                                <RemoveRedEyeRounded />
+                            </div>
+                        </Card>
+                    })
+                }
             </>}
         </>
     </>
