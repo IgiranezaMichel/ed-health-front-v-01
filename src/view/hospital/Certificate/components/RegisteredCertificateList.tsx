7f2a88c0a24bc0c640cc0090a7861a49be26ee40
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-07 17:24:11 +0200

    add preloader

--- src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
+++ src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
@@ -1,14 +1,17 @@
-import { Card } from "@mui/material"
+import { Card, CircularProgress } from "@mui/material"
 import { useFindCertificateByTrainingId_ } from "../../../../controller/viewHooks/Certificate/CertificateQueries";
+import { Certificate } from "../../Certificate";
 
 export const RegisteredCertificateList = (props: { trainingId: number }) => {
     const certificateList=useFindCertificateByTrainingId_(props.trainingId);
     console.log(certificateList.certificateList)
     return <>
         <Card elevation={4} className="border">
-            <div className="fw-bold p-4 text-center">
-                --No Certificate found --
-            </div>
+            {certificateList.isLoading?<div className="fw-bold p-4 text-center">
+                <CircularProgress/>
+            </div>:<>
+            --No Certificate found --
+            </>}
         </Card>
     </>
 }
