45cd9ae3bfbc53e9b597b574d9071ca8cefe935d
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-08 18:35:26 +0200

    add certificate detail query

--- src/controller/viewHooks/Certificate/CertificateQueries.tsx
+++ src/controller/viewHooks/Certificate/CertificateQueries.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery } from "@apollo/client"
-import { _FIND_CERTIFICATE_BY_TRAINING_ID } from "../../../graphQl/queries/CertificateQueries"
+import { _FIND_CERTIFICATE_BY_ID, _FIND_CERTIFICATE_BY_TRAINING_ID } from "../../../graphQl/queries/CertificateQueries"
 import { useEffect, useState } from "react";
 
 export const useFindCertificateByTrainingId_=(trainingId:number)=>{
@@ -20,6 +20,6 @@ useEffect(
 )
 return{certificateList,isLoading,refetch}
 }
-export const useFindCertificateById_=()=>{
-
+export const useFindCertificateById_=(id:number)=>{
+const certificateDetails=useQuery(_FIND_CERTIFICATE_BY_ID);
 }
