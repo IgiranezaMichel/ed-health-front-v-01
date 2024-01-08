1ffef86d0fbcecec0ca91c74760a158d56a41e5c
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-08 17:58:17 +0200

    add certified person total

--- src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
+++ src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Card, CircularProgress, Divider } from "@mui/material"
 import { useFindCertificateByTrainingId_ } from "../../../../controller/viewHooks/Certificate/CertificateQueries";
-import { RemoveRedEyeRounded } from "@mui/icons-material";
+import { People, RemoveRedEyeRounded } from "@mui/icons-material";
 export const RegisteredCertificateList = (props: { trainingId: number }) => {
     const certificateList = useFindCertificateByTrainingId_(props.trainingId);
     console.log(certificateList.certificateList)
@@ -15,7 +15,7 @@ export const RegisteredCertificateList = (props: { trainingId: number }) => {
                         return <Card elevation={4} className="border m-2 p-2">
                             <div className="fs-4 fw-bold" style={{ fontFamily: 'cursive' }}>Certificate {data.title}</div>
                             <div>
-                                
+                                <People/><span >Total certified Person</span> <span className="badge bg-info text-dark fw-bold">{data.certifiedStudentList.length}</span>
                             </div>
                             <Divider />
                             has been approved by <b>{data.accountHolder.name}</b>
