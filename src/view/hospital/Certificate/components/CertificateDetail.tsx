6c20cbfce02a79f23fdd08a25afd36e9a1b64926
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-09 18:42:42 +0200

    when a certificate isLoading

--- src/view/hospital/Certificate/components/CertificateDetail.tsx
+++ src/view/hospital/Certificate/components/CertificateDetail.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react"
+import { useFindCertificateById_ } from "../../../../controller/viewHooks/Certificate/CertificateQueries";
 
 export const CertificateDetail=(props:{certificateId:number})=>{
     const [show,setShow]=useState('');
+    const certificate=useFindCertificateById_(props.certificateId);
     return(
         <>
-        {
+        {certificate.isLoading&&<div>
+            
+        </div>
             
         }
         </>
