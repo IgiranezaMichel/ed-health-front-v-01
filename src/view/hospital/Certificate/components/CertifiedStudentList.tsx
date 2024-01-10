129349fb62e6882fa818a4ad6a2fb413f0d16b0a
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-10 21:06:25 +0200

    add certificateId prop

--- src/view/hospital/Certificate/components/CertifiedStudentList.tsx
+++ src/view/hospital/Certificate/components/CertifiedStudentList.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@mui/material"
+import { useFindCertifiedStudentByCertificateIdPage } from "../../../../controller/viewHooks/CertifiedStudent/CertifiedStudentDao"
 
 export const CertifiedStudentList=(props:{certificateId:number})=>{
-    
+    useFindCertifiedStudentByCertificateIdPage(props.certificateId); 
 return(
     <>
     <Button variant="contained" className="rounded-0 mx-2">
