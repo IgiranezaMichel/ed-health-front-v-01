9427316ce8c1dc3322f4a535d89490e9720fda6b
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-12 20:21:34 +0200

    create buttons

--- src/view/hospital/Certificate/components/CertifiedStudentList.tsx
+++ src/view/hospital/Certificate/components/CertifiedStudentList.tsx
@@ -1,6 +1,17 @@
+import { Button } from "@mui/material"
+
 export const CertifiedStudentList=(props:{certificateId:number})=>{
 return(
     <>
+    <Button className="bg-primary">
+        Certified
+    </Button>
+    <Button>
+        Appending
+    </Button>
+    <Button>
+        Rejection
+    </Button>
     </>
 )
 }
