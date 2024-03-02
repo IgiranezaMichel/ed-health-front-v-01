157bca20d0b4df9183f3d8ad7434d1529d2ed6ea
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-02 19:26:55 +0200

    add training detail

--- src/view/student/training/TrainingDetail.tsx
+++ src/view/student/training/TrainingDetail.tsx
@@ -1,13 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Card } from "@mui/material";
+import { Button, Card } from "@mui/material";
 import { useFindTrainingById } from "../../../controller/viewHooks/training/useFindTrainingById"
-import { Description, LocalHospital, LocationOn } from "@mui/icons-material";
+import { Description, KeyboardArrowDown, LocalHospital, LocationOn } from "@mui/icons-material";
 import { ClockIcon } from "@mui/x-date-pickers";
+import { useState } from "react";
+import { Modal } from "../../../components/default/Modal";
+import { useRegisterStudentTrainingApplication } from "../../../controller/dmlHooks/TrainingApplication/TrainingApplicationDao";
+import { STATUS } from "../../../enums/Status";
 
 export const TrainingDetail = (props: { trainingId: number }) => {
-
-    const { refreshTrainingDetail, trainerList, trainingRequirementList, isFindingTraining, trainingDetail } = useFindTrainingById(Number(props.trainingId));
-    console.log(trainingDetail)
+    const [showDetail,setShowDetail]=useState(false);
+    const studentData=JSON.parse(String(localStorage.getItem("Student")));
+    const studentDataHandler=useRegisterStudentTrainingApplication(studentData.id,props.trainingId,STATUS.APPENDING);
+    const saveStudentTrainingRegistration=()=>{
+        studentDataHandler.saveStudentRegistrationHandler();
+        if(studentDataHandler.hasFinishLoading){
+            refreshTrainingDetail();
+        }
+    }
+    const { refreshTrainingDetail,isFindingTraining, trainingDetail } = useFindTrainingById(Number(props.trainingId));
+    const applicationModal=<Modal id="apply" title="Apply for training" 
+    actionBtn={<Button onClick={()=>saveStudentTrainingRegistration()} className="bg-primary text-white fw-bold"> Yes</Button>}>
+       <div className="py-3 m-2">
+       Are you sure you want to apply this training?
+       </div>
+    </Modal>
     return (
         <div>
             {!isFindingTraining && <>
@@ -27,13 +44,21 @@ export const TrainingDetail = (props: { trainingId: number }) => {
                         <div className="mb-1"><span><LocationOn />{trainingDetail.location.Location.Location.name} || {trainingDetail.location.Location.name} || {trainingDetail.location.name}</span></div>
                         <div className="card-body mb-2">
                             <Description /> {trainingDetail.description}
-                            <span className="float-end"><ClockIcon /> {trainingDetail.deadline}</span>
+                            <span className="float-end" style={{clear:'both'}} ><ClockIcon /> {trainingDetail.deadline}</span>
+                            <div className="text-center fw-bold">
+                            <KeyboardArrowDown onClick={()=>setShowDetail(!showDetail)}/>
+                            </div>
                         </div>
                     </Card>
                     <div className="card-body p-2">
-
-                        {trainingDetail.trainingRequirementList.length != 0 &&
-                            <>
+                        {showDetail&&trainingDetail.trainingRequirementList.length== 0&&<div className="p-3 bg-primary text-center fw-bold text-white">
+                            --- no data found ---
+                        </div>}
+                        {trainingDetail.trainingRequirementList.length!= 0 &&
+                           <>
+                           <small className="p-2">Total applicant <span className="badge bg-info fw-bolder">{trainingDetail.applicantList.length}</span></small>
+                           <small className="p-2">Total Trainer <span className="badge bg-info fw-bolder">{trainingDetail.trainers.length}</span></small>
+                           {showDetail&&<>
                                 <h4 className="mt-3 my-3">Requirement</h4>
                                 {
                                     trainingDetail.trainingRequirementList.map((data: any,index:any) => {
@@ -42,11 +67,15 @@ export const TrainingDetail = (props: { trainingId: number }) => {
                                         </ul>
                                     })
                                 }
-                            </>
+                            </>}
+                           </>
                         }
+                        <Button className="float-end fw-bold text-white" data-bs-toggle="modal"
+                            data-bs-target="#apply" variant="contained">Apply</Button>
                     </div>
                 </Card>
             </>}
+            {applicationModal}
         </div>
     )
 }
