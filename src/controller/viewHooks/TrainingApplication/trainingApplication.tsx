bf8f4003680e2db0e604fbad360d46af84e148d9
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-02 23:39:48 +0200

    add get training applicant page

--- src/controller/viewHooks/TrainingApplication/trainingApplication.tsx
+++ src/controller/viewHooks/TrainingApplication/trainingApplication.tsx
@@ -2,22 +2,42 @@
 import { useEffect, useState } from "react";
 import { PaginationInput } from "../../../typeDefs/PaginationInput";
 import { useQuery } from "@apollo/client";
-import {GET_TRAINING_APPLICANT_PAGE_BY__HOSPITAL_APPROVAL_STATUS } from "../../../graphQl/queries/TrainingApplicationQueries";
+import { GET_STUDENT_TRAINING_APPLICATION_PAGE, GET_TRAINING_APPLICANT_PAGE_BY__HOSPITAL_APPROVAL_STATUS } from "../../../graphQl/queries/TrainingApplicationQueries";
 
-export const useGetTrainingApplicantPage=(status:string,trainingId:number,input:PaginationInput)=>{
-    const {data,refetch}=useQuery(GET_TRAINING_APPLICANT_PAGE_BY__HOSPITAL_APPROVAL_STATUS,{variables:{status:status,trainingId:trainingId,input:input}});
-    const [trainingApplicants,setTrainingApplicants]=useState<any>([]);
-    const [isLoadingApplicant,setIsLoadingApplicant]=useState(true);
+export const useGetTrainingApplicantPage = (status: string, trainingId: number, input: PaginationInput) => {
+    const { data, refetch } = useQuery(GET_TRAINING_APPLICANT_PAGE_BY__HOSPITAL_APPROVAL_STATUS, { variables: { status: status, trainingId: trainingId, input: input } });
+    const [trainingApplicants, setTrainingApplicants] = useState<any>([]);
+    const [isLoadingApplicant, setIsLoadingApplicant] = useState(true);
     useEffect(
-        ()=>{
-           const fetchData=async()=>{
+        () => {
+            const fetchData = async () => {
                 return await data;
             }
-            fetchData().then((data)=>{setTrainingApplicants(data.getTrainingApplicantPageByHospitalApprovalStatus);setIsLoadingApplicant(false)}).catch(err=>err)
+            fetchData().then((data) => { setTrainingApplicants(data.getTrainingApplicantPageByHospitalApprovalStatus); setIsLoadingApplicant(false) }).catch(err => err)
         }
     )
-    const refetchApplicants=()=>{
+    const refetchApplicants = () => {
         refetch();
     }
-    return{refetchApplicants,trainingApplicants,isLoadingApplicant}
-    }
+    return { refetchApplicants, trainingApplicants, isLoadingApplicant }
+}
+export const useGetStudentTrainingApplicationPage = (studentId: number, status: string, input: PaginationInput) => {
+    const [hasFinishLoading, setHasFinishLoading] = useState(false);
+    const [trainingApplicationDetail, setTrainingApplicationDetail] = useState<any>({});
+    const { data, refetch } = useQuery(GET_STUDENT_TRAINING_APPLICATION_PAGE,
+        { variables: { studentId: studentId, status: status, input: input } });
+    useEffect(
+        () => {
+            const fetchData = async () => {
+                return await data;
+            }
+            fetchData()
+                .then(data => {
+                    setTrainingApplicationDetail(data.getStudentTrainingApplicationPage);
+                    setHasFinishLoading(true);
+                })
+                .catch(err => err);
+        }
+    )
+    return { trainingApplicationDetail, refetch, hasFinishLoading }
+}
