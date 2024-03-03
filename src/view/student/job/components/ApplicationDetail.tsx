1e33b773a362f3c318592a4438af8a26fa279e67
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-03 18:13:15 +0200

    add save job application

--- src/view/student/job/components/ApplicationDetail.tsx
+++ src/view/student/job/components/ApplicationDetail.tsx
@@ -2,11 +2,23 @@
 import { Button, Card, Divider } from "@mui/material";
 import { useFindJobById } from "../../../../controller/viewHooks/jobHooks";
 import { LocationOn } from "@mui/icons-material";
+import { Modal } from "../../../../components/default/Modal";
+import { useRegisterStudentJobApplication } from "../../../../controller/dmlHooks/JobApplication/JobApplicationController";
+import { STATUS } from "../../../../enums/Status";
 export const ApplicationDetail=(props:{id:number})=>{
 const job=useFindJobById(Number(props.id));
+const student=JSON.parse(String(localStorage.getItem('Student')));
+const{result,saveJobApplicationHandler}=useRegisterStudentJobApplication(Number(student.id),job.jobDetail.id,STATUS.APPENDING);
+const saveJobApplication=()=>{
+    saveJobApplicationHandler().then(()=>alert(result));
+}
 console.log(job.jobDetail)
+const saveApplicationModal=<Modal id="saveApplication" title="Job Application" actionBtn={<button className="p-2 fw-bold text-white bg-primary border-0" onClick={()=>saveJobApplication()}>Yes</button>}>
+<div className="p-3">Are you sure you want to apply for this job</div>
+    </Modal>
 return(
     <>
+    
     {!job.jobDetailIsLoading&&
     <Card elevation={4} className="p-3">
         <div className="fs-4 fw-bold">
@@ -35,10 +47,11 @@ return(
             }</div>
        }
        <div className="modal-footer">
-        <Button variant="contained">Apply</Button>
+        <Button variant="contained"  data-bs-toggle="modal"
+            data-bs-target="#saveApplication">Apply</Button>
        </div>
     </Card>}
-
+{saveApplicationModal}
     </>
 )
 }
