eef7cfb88940fe5ba67bfdb4851d6ba68d77f472
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-03 18:12:40 +0200

    add register job application

--- src/graphQl/mutation/JobApplication.tsx
+++ src/graphQl/mutation/JobApplication.tsx
@@ -2,6 +2,6 @@ import { gql } from "@apollo/client";
 
 export const REGISTER_JOB_APPLICATION=gql`
 mutation($studentId:Long,$jobId:Long,$status:String){
-registerJobApplication(studentId:$studentId,jobId:$jobId,status:$status)
+registerStudentJobApplication(studentId:$studentId,jobId:$jobId,status:$status)
 }
 `
