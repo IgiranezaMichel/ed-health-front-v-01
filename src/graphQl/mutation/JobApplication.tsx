d8ed780aea590115cc580a7775a41d936817145e
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-03 22:09:54 +0200

    create change application status by hospital admin

--- src/graphQl/mutation/JobApplication.tsx
+++ src/graphQl/mutation/JobApplication.tsx
@@ -5,3 +5,8 @@ mutation($studentId:Long,$jobId:Long,$status:String){
 registerStudentJobApplication(studentId:$studentId,jobId:$jobId,status:$status)
 }
 `
+export const CHANGE_JOB_APPLICATION_STATUS_BY_HOSPITAL_ADMIN=gql`
+mutation($id:Long,$status:String){
+    changeJobApplicantStatusByHospitalAdmin(id:$id,status:$status)
+}
+`
