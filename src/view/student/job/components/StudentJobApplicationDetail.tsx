291547cd3740fb5652ab28fc48128238b3ab473e
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-03 18:14:08 +0200

    redefine card attribute

--- src/view/student/job/components/StudentJobApplicationDetail.tsx
+++ src/view/student/job/components/StudentJobApplicationDetail.tsx
@@ -16,16 +16,16 @@ export const StudentJobApplicationDetail=()=>{
         {studentJobApplication.isLoading&&<div><CircularProgress/></div>}
       {!studentJobApplication.isLoading&&
       studentJobApplication.jobApplicationObj.content.map((data:any,index:any)=>{
-    return <Card key={index} elevation={4} className="mb-3 border border-1">
+    return <Card key={index} elevation={4} className="mb-3 border p-3 border-1">
         <div className="display-6">{data.job.title}</div>
         <div className="mb-2"><LocationOn/>{data.job.hospital.location.Location.Location.name}/{data.job.hospital.location.Location.name}{data.job.hospital.location.name}</div>
-        <div className="mb-2">
-            <span className="float-md-end">Posted by {data.job.hospital.name}</span>
+        <div className="mb-2 text-md-end">
+            <span className="">Posted by {data.job.hospital.name}</span>
         </div>
         <Divider className="border border-2"/>
         <div className="">{data.job.description}</div>
-        <div className=""><b>Deadline </b>{data.job.deadline}</div>
-        <div className=""><b>Posted on </b>{data.timeStamp}</div>
+        <div className=""><b>Deadline </b>{String(data.job.deadline).split('T')[0]}</div>
+        <div className=""><b>Posted on </b>{String(data.timeStamp).split('T')[0]}</div>
       </Card>})}
       {
         !studentJobApplication.isLoading&&studentJobApplication.jobApplicationObj.content.length==0&&<div className="fw-bold text-white text-center bg-primary p-3">-- No data found -- </div>
