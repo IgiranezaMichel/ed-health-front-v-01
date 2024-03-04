af6319c14a074cf6cb749efb1711538dac372a11
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-04 23:19:42 +0200

    call useGetAllStudentCertificatePage hook

--- src/view/student/certificate/index.tsx
+++ src/view/student/certificate/index.tsx
@@ -4,7 +4,13 @@ import { Navigation } from "../../../components/default/Navigation"
 import { Modal } from "../../../components/default/Modal"
 import QrCode from "../../../components/default/QrCode"
 import { Button, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material"
+import { useGetAllStudentCertificatePage } from "../../../controller/viewHooks/CertifiedStudent/CertifiedStudentDao"
+import { useState } from "react"
+import { PaginationInput } from "../../../typeDefs/PaginationInput"
 export const StudentCertificate = () => {
+    const student=JSON.parse(String(localStorage.getItem("Student")));
+    const [page,setPage]=useState<PaginationInput>({pageNumber:0,pageSize:10,sort:"id"});
+    useGetAllStudentCertificatePage(Number(student.id),page);
     const certificateModal = <Modal id="certificate" title="My certificate qrcode" actionBtn={<button data-bs-toggle="modal"
         data-bs-dismiss="#certificate" className="bg-danger text-white fw-bold border-0 p-1">close</button>}>
         <div className="text-center p-2">
@@ -41,6 +47,7 @@ export const StudentCertificate = () => {
                     </TableRow>
                 </TableBody>
             </Table>
+            
             {certificateModal}
         </Navigation>
     )
