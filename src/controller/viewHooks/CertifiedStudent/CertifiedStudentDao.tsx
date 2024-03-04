335e91e244a139c704eaf366148dcae42f5368bf
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-04 23:18:57 +0200

    create useGetAllStudentCertificatePage hook

---
+++ src/controller/viewHooks/CertifiedStudent/CertifiedStudentDao.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
+import { PaginationInput } from "../../../typeDefs/PaginationInput";
+import {useQuery } from "@apollo/client";
+import { GET_STUDENT_CERTIFICATE_PAGE } from "../../../graphQl/queries/CertifiedStudent";
+
+export const useGetAllStudentCertificatePage=(studentId:number,input:PaginationInput)=>{
+const [studentCertificateObj,setStudentCertificateObj]=useState<any>({});
+const [isLoading,setIsLoading]=useState(true);
+const {data,refetch}=useQuery(GET_STUDENT_CERTIFICATE_PAGE,{variables:{studentId:studentId,input:input}});
+useEffect(
+    ()=>{
+        const fetchData=async()=>{
+            return await data;
+        }
+        fetchData().then(data=>{
+            setStudentCertificateObj(data.getStudentCertificatePage)
+            setIsLoading(false);
+        })
+    }
+)
+return{isLoading,refetch,studentCertificateObj}
+}
