a956ba4764630b6e835fe20cac0ee4b05ffdc072
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-05 12:54:41 +0200

    add content retriaval queries

--- src/graphQl/queries/CertifiedStudent.tsx
+++ src/graphQl/queries/CertifiedStudent.tsx
@@ -3,6 +3,16 @@ import { gql } from "@apollo/client";
 export const GET_STUDENT_CERTIFICATE_PAGE=gql`
 mutation($studentId:Long,$input:PaginationInput){
     getStudentCertificatePage(studentId:$studentId,input:$input){
+        pageNumber
+    totalPages
+    size
+    content{
+      certificate{
+        title
+        description
+        timeStamp
+      }
+    }
     }
 }
 `
