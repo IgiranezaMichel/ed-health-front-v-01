790511502bf4c8070a7c640861ffc2691fd27559
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-05 17:54:05 +0200

    document formatting

--- src/graphQl/mutation/TrainingApplicationMutations.tsx
+++ src/graphQl/mutation/TrainingApplicationMutations.tsx
@@ -1,11 +1,11 @@
 import { gql } from "@apollo/client";
 
-export const CHANGE_TRAINING_APPLICATION_STATUS_BY_HOSPITAL_ADMIN=gql`
+export const CHANGE_TRAINING_APPLICATION_STATUS_BY_HOSPITAL_ADMIN = gql`
 mutation($trainingId:Long,$trainingStatus:String){
-    changeApplicantStatusByHospitalAdmin(trainingId:$trainingId,trainingStatus:$trainingStatus)
+    changeApplicantStatusByHospitalAdmin(applicationId:$trainingId,applicationStatus:$trainingStatus)
 }
 `
-export const REGISTER_STUDENT_TRAINING_APPLICATION=gql`
+export const REGISTER_STUDENT_TRAINING_APPLICATION = gql`
 mutation($studentId:Long,$trainingId:Long,$studentApprovalStatus:String){
 registerStudentTrainingApplication(studentId:$studentId,trainingId:$trainingId,studentApprovalStatus:$studentApprovalStatus)
 }
