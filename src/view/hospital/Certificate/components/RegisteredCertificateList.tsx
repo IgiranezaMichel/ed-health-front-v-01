5fff74345f7f2bf95e1015cacb5b84c4452169dc
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-06 14:42:20 +0200

    create register certificate list

--- src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
+++ src/view/hospital/Certificate/components/RegisteredCertificateList.tsx
@@ -1,7 +1,11 @@
 import { Card } from "@mui/material"
 
-export const RegisteredCertificateList=()=>{
-    return<Card>
-        
+export const RegisteredCertificateList=(props:{trainingId:number})=>{
+    return<>
+    <Card elevation={4} className="border">
+       <div className="fw-bold p-4 text-center">
+       --No Certificate found --
+       </div>
     </Card>
+    </>
 }
