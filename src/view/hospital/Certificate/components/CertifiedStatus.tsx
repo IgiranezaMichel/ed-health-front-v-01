2e2de28833a667410c25146bda37fbf93652f9f4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-07 12:12:05 +0200

    import useGetCertifiedStudentByAdminApprovalStatus

--- src/view/hospital/Certificate/components/CertifiedStatus.tsx
+++ src/view/hospital/Certificate/components/CertifiedStatus.tsx
@@ -1,4 +1,8 @@
+import { useGetCertifiedStudentByAdminApprovalStatus } from "../../../../controller/viewHooks/TrainingApplication/trainingApplication"
+
 export const CertifiedStatus=()=>{
+    
+    useGetCertifiedStudentByAdminApprovalStatus()
     return(
         <>
         
