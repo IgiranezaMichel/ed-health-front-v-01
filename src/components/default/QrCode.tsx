024f4c413c7e92cac452f6add53f9a98d1394201
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-07 10:09:21 +0200

    add url in qrcode

--- src/components/default/QrCode.tsx
+++ src/components/default/QrCode.tsx
@@ -2,10 +2,10 @@
 import { CSSProperties } from "react";
 import QRCode from "react-qr-code";
 
-export default function QrCode(props:{certificateId:number,style:CSSProperties}) {
+export default function QrCode(props:{certificateId:number,style:CSSProperties,url:string}) {
   return (
     <div>
-       <QRCode style={props.style} value={'localhost:/check-certificate/'+props.certificateId+''}/>
+       <QRCode style={props.style} value={props.url+props.certificateId+''}/>
     </div>
   )
 }
