a02aeda393b03c8793871cd897f758f25592697c
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-07 14:35:12 +0200

    add events

--- src/view/hospital/Certificate/components/CertifiedStudentList.tsx
+++ src/view/hospital/Certificate/components/CertifiedStudentList.tsx
@@ -4,16 +4,16 @@ import { CertifiedStudents } from "./CertifiedStudents";
 import { CertifiedStatus } from "./CertifiedStatus";
 import { useState } from "react";
 export const CertifiedStudentList=(props:{certificateId:number,trainingId:number})=>{
-    const [status,setStatus]=useState('approved');
+    const [status,setStatus]=useState('certificate');
 return(
     <>
-    <Button onClick={()=>setStatus('')} variant="contained" className="rounded-0 mx-2">
+    <Button onClick={()=>setStatus('certificate')} variant="contained" className="rounded-0 mx-2">
         Certified
     </Button>
-    <Button variant="outlined" className="rounded-0">
+    <Button onClick={()=>setStatus('approved')} variant="outlined" className="rounded-0">
         Appending
     </Button>
-    <Button variant="outlined" className=" mx-1 rounded-0">
+    <Button onClick={()=>setStatus('rejected')} variant="outlined" className=" mx-1 rounded-0">
         Rejection
     </Button>
    <CertifiedStudents certificateId={props.certificateId}/>
