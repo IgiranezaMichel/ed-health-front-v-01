66a72593e3e5b8f1059d25bf58cf54e0a242d331
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-07 12:15:55 +0200

    add parameters in hook

--- src/view/hospital/Certificate/components/CertifiedStatus.tsx
+++ src/view/hospital/Certificate/components/CertifiedStatus.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { useGetCertifiedStudentByAdminApprovalStatus } from "../../../../controller/viewHooks/TrainingApplication/trainingApplication"
 
 export const CertifiedStatus=(props:{status:string,trainingId:long})=>{
-   const certifiedStudent=useGetCertifiedStudentByAdminApprovalStatus()
+   const certifiedStudent=useGetCertifiedStudentByAdminApprovalStatus(props.status,props.trainingId,page);
     return(
         <>
         
