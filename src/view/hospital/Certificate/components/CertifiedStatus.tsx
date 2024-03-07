4df4d2e14425ac374039827fb9083aca18ff9fd4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-07 15:49:00 +0200

    populate user information

--- src/view/hospital/Certificate/components/CertifiedStatus.tsx
+++ src/view/hospital/Certificate/components/CertifiedStatus.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useGetCertifiedStudentByAdminApprovalStatus } from "../../../../controller/viewHooks/TrainingApplication/trainingApplication"
 import { PaginationInput } from "../../../../typeDefs/PaginationInput";
 import { Card, CircularProgress } from "@mui/material";
+import { Email, Person, Wc } from "@mui/icons-material";
 
 export const CertifiedStatus=(props:{status:string,trainingId:number})=>{
     const [page,setPage]=useState<PaginationInput>({pageNumber:0,pageSize:10,sort:"id"});
@@ -20,9 +21,15 @@ export const CertifiedStatus=(props:{status:string,trainingId:number})=>{
                 </Card>:<div>
                     {
                         certifiedStudents.certifiedStudentDetailObj.content.map((data:any)=>{
-                            return <Card >
+                            return <Card className="row col-12 m-auto">
                                 <div className="col-sm-2">
-                                    <img src={data.student.user.profilePicture} alt="" />
+                                    <img src={data.student.user.profilePicture} className="card-img" />
+                                </div>
+                                <div className="col-md-5">
+                                    <div className="mb-2"><Person/> {data.student.user.name}</div>
+                                    <div className="mb-2"><Email/> {data.student.user.email}</div>
+                                    <div className="mb-2"><Wc/> {data.student.user.gender}</div>
+
                                 </div>
                             </Card>
                         })
