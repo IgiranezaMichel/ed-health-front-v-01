f30f22c630b73868bd98983f9b72ca41bc440365
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-07 12:23:01 +0200

    add consoleadd card

--- src/view/hospital/Certificate/components/CertifiedStatus.tsx
+++ src/view/hospital/Certificate/components/CertifiedStatus.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useGetCertifiedStudentByAdminApprovalStatus } from "../../../../controller/viewHooks/TrainingApplication/trainingApplication"
 import { PaginationInput } from "../../../../typeDefs/PaginationInput";
-import { CircularProgress } from "@mui/material";
+import { Card, CircularProgress } from "@mui/material";
 
 export const CertifiedStatus=(props:{status:string,trainingId:number})=>{
     const [page,setPage]=useState<PaginationInput>({pageNumber:0,pageSize:10,sort:"id"});
@@ -14,7 +14,9 @@ export const CertifiedStatus=(props:{status:string,trainingId:number})=>{
             </div>
             :
             <div>
-                
+                <Card>
+
+                </Card>
             </div>
         }
         </>
