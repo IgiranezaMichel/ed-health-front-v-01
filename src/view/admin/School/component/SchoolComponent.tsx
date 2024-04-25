5193d37d82d1758f63ce72a0c1d37715420a3f38
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-25 21:40:57 +0200

    add new hospital

--- src/view/admin/School/component/SchoolComponent.tsx
+++ src/view/admin/School/component/SchoolComponent.tsx
@@ -66,7 +66,7 @@ export const SchoolComponent = (props: { accessedBy: string,children:ReactNode }
                     </div>
                 </div>
                 <Dialog maxWidth='sm' PaperProps={{className:'rounded-0'}} open={addNewHospital}>
-                    <div className="p-3 sticky-top bg-white">Add new school <Close className="float-end"/></div>
+                    <div className="p-3 sticky-top bg-white">Add new school <Close onClick={()=>setAddNewHospital(false)} className="float-end"/></div>
                     <SchoolFormModal/>
                 </Dialog>
             </div>}
