7ff174b7f5bd7a01add2194ac2e249a85bb815f4
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-25 21:40:30 +0200

    find user by email

--- src/graphQl/mutation/UserMutations.tsx
+++ src/graphQl/mutation/UserMutations.tsx
@@ -1,11 +1,24 @@
 import { gql } from "@apollo/client";
 
-export const REGISTER_USER=gql`
+export const REGISTER_USER = gql`
 mutation($input:AccountHolderInput){
     registerUser(input:$input){
     id
     name
     gender
   }
+}
+`
+export const FIND_USER_BY_EMAIL = gql`
+mutation($email:String)
+{
+  findAccountHolderByEmail(email:$email){
+  id
+  name
+  gender
+  phoneNumber
+  dob
+  profilePicture
+}
 }
 `
