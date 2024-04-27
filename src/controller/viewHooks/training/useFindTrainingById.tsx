42057b09273aecade19e99f1e7b9cda338a58a2f
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-27 20:57:05 +0200

    add find training data

--- src/controller/viewHooks/training/useFindTrainingById.tsx
+++ src/controller/viewHooks/training/useFindTrainingById.tsx
@@ -8,17 +8,17 @@ export const useFindTrainingById=(id:number)=>{
     const [isFindingTraining,setIsFindingTraining]=useState(true);
     const [trainingDetail,setTrainingDetail]=useState<any>({});
     const [applicantList,setApplicantList]=useState<any>([]);
-    const [trainingRequirementList,setTrainingRequirementList]=useState<any>([]);
     const [trainerList,setTrainerList]=useState<any>([]);
     useEffect(
      ()=>{
          const fetchData=async()=>{
+           if(findTraining.data){
             return await findTraining.data.findTrainingById;
+           }
          }
          fetchData().then(data=>{
             setTrainingDetail(data);
             setApplicantList(data.applicantList);
-            setTrainingRequirementList(data.trainingRequirementList);
             setTrainerList(data.trainers);
             setIsFindingTraining(false);
          }).catch(err=>console.log(err));
@@ -27,5 +27,5 @@ export const useFindTrainingById=(id:number)=>{
     const refreshTrainingDetail=()=>{
       findTraining.refetch();
     }
-    return{isFindingTraining,trainerList,trainingDetail,applicantList,trainingRequirementList,refreshTrainingDetail}
+    return{isFindingTraining,trainerList,trainingDetail,applicantList,refreshTrainingDetail}
  }
