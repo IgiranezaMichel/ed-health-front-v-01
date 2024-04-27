fa722ceda0d170a4bdaa3a6f48d6c89bef60308c
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-27 23:19:19 +0200

    add modal footer

--- src/view/student/job/index.tsx
+++ src/view/student/job/index.tsx
@@ -25,37 +25,29 @@ export const JobPublished = () => {
     return (
         <Navigation items={StudentMenu}>
             {jobId == 0 && <>
-                <Card elevation={4} className="col-sm-6 float-end" style={{ clear: 'both' }}>
-                    <DashboardCard icon={<BusinessCenter className="float-md-end fs-1" />} subtitleDescription="Job successful analysis" />
-                    <div className="col-12 py-1">
-                        <PIE_CHART_DEFAULT centerLabel="Job" items={[{ value: 1, label: 'Success' }, { value: 1, label: 'Fail' }, { value: 1, label: 'Middle' }]} />
-                    </div>
-                </Card>
-                <div className="p-1" style={{ clear: 'both' }}>
+                <div className="p-1 modal-footer" style={{ clear: 'both' }}>
                     <Button onClick={() => setShow('newJobs')} variant={show == 'newJobs' ? "contained" : "outlined"} className="m-1">New Jobs</Button>
                     <Button onClick={() => setShow('history')} variant={show == 'history' ? "contained" : "outlined"} className="m-1">history</Button>
                 </div>
-                <div className="fw-bold py-2" style={{ clear: 'both' }}>
-                    <NewReleases />Recent posted Jobs
+                <div className="fw-bold py-2 display-6" style={{ clear: 'both' }}>
+                    <NewReleases className="fs-1" />Recent posted Jobs
                 </div>
                 <section>
                     {jobs.finishLoading && jobs.jobObj.content.length != 0 && show == 'newJobs' && jobs.jobObj.content.map((job: any, index: any) => {
-                        return <Card key={index} className="p-2 border">
+                        return <Card key={index} className="p-2 border mb-2 rounded-0">
                             <div className="fs-4">{job.title}</div>
                             <div>
-                                <LocationOn /> {job.hospital.location.Location.Location.name}/{job.hospital.location.Location.name}/{job.hospital.location.name}
+                                <LocationOn /> {job.hospital.location.Location.Location.name} || {job.hospital.location.Location.name} || {job.hospital.location.name}
                             </div>
                             <div><People /> <b>N <sup className="border">o</sup> of Employee</b> {job.numberOfEmployee}</div>
                             <div style={{ clear: 'both' }}>
-                                <div ><span><TimeIcon />On {String(job.timeStamp).split('T')[0]}</span></div>
-                                <div className="text-md-end" ><LocalHospital /> <b>Posted by </b> {job.hospital.name}</div></div>
-
+                                <div className="text-md-end" ><LocalHospital /> <b>Posted by </b> {job.hospital.name}</div>
+                            </div>
                             <Divider className="m-2 m-2 border border-1 border-dark" />
-
                             <div>
                                 {job.description}
                                 <div className="text-md-end">
-                                    Deadline <TimeIcon /> {String(job.deadline).split('T')[0]}
+                                    <b>Deadline</b>  {String(job.deadline).split('T')[0]}
                                 </div>
                                 <div className="modal-footer">
                                     <Button onClick={() => setJobId(Number(job.id))} className="bg-primary text-white fw-bold">Apply</Button>
