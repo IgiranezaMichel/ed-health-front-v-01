0ca42b03cc5067d2e8206429bb7e0e6baa07d644
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-27 23:18:41 +0200

    add native select

--- src/view/ncnm/Training/components/ExpiredTrainingList.tsx
+++ src/view/ncnm/Training/components/ExpiredTrainingList.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { CalendarToday, Close, LocalHospital, LocationOn, School, Sort } from "@mui/icons-material";
-import { Dialog, Pagination, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { CalendarToday, Close, LocationOn, School, Sort } from "@mui/icons-material";
+import { Avatar, Dialog, NativeSelect, Pagination, Table, TableBody, TableCell, TableHead, TableRow, Tooltip } from "@mui/material";
 import { FC, useState } from "react";
 import { useExpiredTraining } from "../../../../controller/viewHooks/training/useExpiredTraining";
 import { PaginationInput } from "../../../../typeDefs/PaginationInput";
@@ -16,33 +16,33 @@ export const ExpiredTrainingList: FC<trainingControl> = (props) => {
         event;
         setPage({ ...page, pageNumber: value - 1 });
     };
-    const [trainingId,setTrainingId]=useState(0);
-    const [openTrainingDetail,setOpenTrainingDetail]=useState(false);
+    const [trainingId, setTrainingId] = useState(0);
+    const [openTrainingDetail, setOpenTrainingDetail] = useState(false);
     return (
         <>
-            <div className="mt-4">  Page {expiredTrainingDetail.pageNumber + 1} out of {expiredTrainingDetail.totalPages}  <span>
-                <select onChange={(e) => setPage({ ...page, pageSize: Number(e.target.value) })} className="p-1 mx-2">
+            {expiredTrainingDetail.totalPages != 0 && <div className="mt-4">  Page {expiredTrainingDetail.pageNumber + 1} out of {expiredTrainingDetail.totalPages}  <span>
+                <NativeSelect onChange={(e) => setPage({ ...page, pageSize: Number(e.target.value) })} className="p-1 mx-2">
                     <option value="8">8</option>
                     <option value="16">18</option>
                     <option value="24">24</option>
-                </select>
+                </NativeSelect>
             </span>
-                <span className="float-end"> Sort by<select onChange={e => setPage({ ...page, sort: e.target.value })} className="custom-select p-1" name="" id="">
+                <span className="float-end"> Sort by<Sort /><NativeSelect onChange={e => setPage({ ...page, sort: e.target.value })} className="custom-select p-1" name="" id="">
                     <option selected={page.sort == 'description' ? true : false} value="description">Description</option>
                     <option selected={page.sort == 'deadline' ? true : false} value="deadline">Deadline</option>
                     <option selected={page.sort == 'location' ? true : false} value="location">Location</option>
-                </select><Sort /></span>
+                </NativeSelect></span>
                 <Pagination count={expiredTrainingDetail.totalPages} onChange={handleChange} page={expiredTrainingDetail.pageNumber + 1} />
-            </div>
-            <Table className="overflow-auto">
+            </div>}
+            <Table className="overflow-auto mt-4">
                 <TableHead>
-                    <TableRow>
-                        <TableCell>#</TableCell>
-                        <TableCell>Hospital</TableCell>
-                        <TableCell>Title</TableCell>
-                        <TableCell>Published_On</TableCell>
-                        <TableCell>Deadline</TableCell>
-                        {props.status != 'cancelled' && <TableCell>Action</TableCell>}
+                    <TableRow className={props.status == 'cancelled'?"bg-danger":"bg-success"}>
+                        <TableCell className="fw-bold text-white">#</TableCell>
+                        <TableCell className="fw-bold text-white">Hospital</TableCell>
+                        <TableCell className="fw-bold text-white">Title</TableCell>
+                        <TableCell className="fw-bold text-white">Published_On</TableCell>
+                        <TableCell className="fw-bold text-white">Deadline</TableCell>
+                        {props.status != 'cancelled' && <TableCell className="fw-bold text-white">Action</TableCell>}
                     </TableRow>
                 </TableHead>
                 {!isLoadingExpiredTrainingDatas && <TableBody>
@@ -53,29 +53,36 @@ export const ExpiredTrainingList: FC<trainingControl> = (props) => {
                                     <TableCell>
                                         {index + 1}
                                     </TableCell>
-                                    <TableCell className="col-sm-2">
-                                        <div><img className="card-img" src={data.hospital.logo} alt="" /></div>
-                                        <div><LocalHospital />{data.hospital.name}</div>
+                                    <TableCell >
+                                        <section className="d-flex">
+                                        <Tooltip title={<div><img src={data.hospital.logo} alt="" /></div>}><Avatar src={data.hospital.logo}/></Tooltip>
+                                        <div className="card justify-content-center border-0">{data.hospital.name}</div>
+                                        </section>
                                     </TableCell>
                                     <TableCell>{data.title}</TableCell>
                                     <TableCell><CalendarToday />{String(data.timeStamp).split('T')[0]}</TableCell>
                                     <TableCell>
-                                        <div> <CalendarToday />{String(data.deadline).split('T')[0]}</div>
+                                        <div className="mb-3"> <CalendarToday />{String(data.deadline).split('T')[0]}</div>
                                         <div><LocationOn />{data.location.Location.Location.name}/{data.location.Location.name}/{data.location.name}</div>
                                     </TableCell>
                                     <TableCell>
-                                        {props.status != 'cancelled' && <School onClick={()=>{setTrainingId(data.id);setOpenTrainingDetail(true)}}/>}
+                                        {props.status != 'cancelled' && <School onClick={() => { setTrainingId(data.id); setOpenTrainingDetail(true) }} />}
                                     </TableCell>
                                 </TableRow>
                             )
                         })
                     }
+                    {expiredTrainingDetail != undefined && expiredTrainingDetail.content != undefined && expiredTrainingDetail.content.length == 0 && <TableRow>
+                        <TableCell colSpan={5} className="text-center">
+                            -- no data found --
+                        </TableCell>
+                    </TableRow>}
                 </TableBody>}
             </Table>
-            <Dialog maxWidth='lg' PaperProps={{className:'col-12 rounded-0'}} open={openTrainingDetail}>
-            <SuccessTrainingDetail trainingId={trainingId}>
-                <div className="p-4 bg-white sticky-top fw-bold">Training Details <Close onClick={()=>setOpenTrainingDetail(false)} className="float-end"/></div>
-            </SuccessTrainingDetail>
+            <Dialog maxWidth='lg' PaperProps={{ className: 'col-12 rounded-0' }} open={openTrainingDetail}>
+                <SuccessTrainingDetail trainingId={trainingId}>
+                    <div className="p-4 bg-white sticky-top fw-bold">Training Details <Close onClick={() => setOpenTrainingDetail(false)} className="float-end" /></div>
+                </SuccessTrainingDetail>
             </Dialog>
         </>
     )
