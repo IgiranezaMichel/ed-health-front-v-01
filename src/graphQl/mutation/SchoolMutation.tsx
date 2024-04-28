c8dfbb9aab714284f238134567f82ad0f09018a3
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-28 20:06:54 +0200

    add school input

--- src/graphQl/mutation/SchoolMutation.tsx
+++ src/graphQl/mutation/SchoolMutation.tsx
@@ -1,7 +1,12 @@
 import { gql } from "@apollo/client";
 
-export const REGISTER_SCHOOL=gql`
-mutation($input:SchoolInput){
-    registerSchool(input:$input)
+export const REGISTER_SCHOOL_FOR_EXISTING_ACCOUNT_HOLDER=gql`
+mutation($schoolInput:SchoolInput,$userEmail:String){
+    registerSchoolForExistingAccountHolder(schoolInput:$schoolInput,userEmail:$userEmail)
+}
+`
+export const REGISTER_SCHOOL_FOR_NEW_ACCOUNT_HOLDER=gql`
+mutation($schoolInput:SchoolInput,$userInput:AccountHolderInput){
+    registerSchoolForNewAccountHolder(schoolInput:$schoolInput,userInput:$userInput)
 }
 `
