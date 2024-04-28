a62da43f0fa5d68fb95d144a9b89bc80c8993dc4
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-28 16:18:18 +0200

    add refetch

--- src/controller/viewHooks/Location/useFilterLocation.tsx
+++ src/controller/viewHooks/Location/useFilterLocation.tsx
@@ -3,18 +3,21 @@ import { useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { FILTER_LOCATION_BY_TYPE } from "../../../graphQl/queries/locationQueries";
 
-export const useFilterLocation=(sort:string,type:string)=>{
-    const [listOfLocationData,setListOfLocationData]=useState<any>([]);
-    const[isLoading,setIsLoading]=useState(true);
-    const locationData=useQuery(FILTER_LOCATION_BY_TYPE,{variables:{sort:sort,type:type}})
-useEffect(
-    ()=>{
-        const fetchFilterLocation=async()=>{
-             
-           return await locationData.data;
+export const useFilterLocation = (sort: string, type: string) => {
+    const [listOfLocationData, setListOfLocationData] = useState<any>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const locationData = useQuery(FILTER_LOCATION_BY_TYPE, { variables: { sort: sort, type: type } })
+    useEffect(
+        () => {
+            const fetchFilterLocation = async () => {
+
+                return await locationData.data;
+            }
+            fetchFilterLocation().then(data => { setIsLoading(false); setListOfLocationData(data.filterLocationByType) }).catch(err => err)
         }
-        fetchFilterLocation().then(data=>{setIsLoading(false);setListOfLocationData(data.filterLocationByType)}).catch(err=>err)
+    )
+    const refetch=async()=>{
+        locationData.refetch();
     }
-)
-return {listOfLocationData,isLoading}
+    return { listOfLocationData, isLoading,refetch }
 }
