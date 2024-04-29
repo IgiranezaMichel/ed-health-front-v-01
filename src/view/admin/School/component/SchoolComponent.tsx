d141f1fd01740e8351acf34638bd4f36ce033bb3
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-29 14:48:37 +0200

    add new school

--- src/view/admin/School/component/SchoolComponent.tsx
+++ src/view/admin/School/component/SchoolComponent.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Close, DataSaverOn, DeleteOutlineRounded, Groups2, ListAlt, LockPersonRounded, PinDrop, School, SchoolSharp } from "@mui/icons-material";
+import { Close, DataSaverOn, DeleteOutlineRounded,ListAlt,PinDrop, School, SchoolSharp } from "@mui/icons-material";
 import { Button, Card,Dialog, Skeleton, Tooltip } from "@mui/material";
 import { DashboardCard } from "../../../../components/default/DashboardCard";
 import { SchoolFormModal } from "./schoolModal";
@@ -8,8 +8,9 @@ import { useNavigate } from "react-router-dom";
 import { useHospitalContext } from "../../../../context/hospitalContext";
 export const SchoolComponent = (props: { accessedBy: string,children:ReactNode }) => {
     const navigation = useNavigate();
-    const [addNewHospital,setAddNewHospital]=useState(false);
+    const [addNewSchool,setAddNewSchool]=useState(false);
     const {data}=useHospitalContext();
+    console.log(data)
       return (
         <>
             {data.schoolDataIsLoading && <div className="col-sm-8 m-auto">
@@ -24,11 +25,11 @@ export const SchoolComponent = (props: { accessedBy: string,children:ReactNode }
             {!data.schoolDataIsLoading && <div>
                 <div className="row container-lg m-auto">
                     <span className="display-6 fw-bold mb-3" style={{ filter: 'drop-shadow(2px 3px grey)' }}>Manage School</span>
-                    <section className="col-sm-4">
+                    <section className="col-md-4">
                         <DashboardCard size={data.schoolSize} title="Total Schools" subtitleDescription="Total Schools" icon={<School />} />
                     </section>
                     <div className="container-lg mt-3">
-                        <Button className="rounded-0 bg-primary text-white" onClick={()=>setAddNewHospital(true)}><DataSaverOn />
+                        <Button className="rounded-0 bg-primary text-white" onClick={()=>setAddNewSchool(true)}><DataSaverOn />
                         </Button>
                     </div>
                     {props.children}
@@ -41,16 +42,12 @@ export const SchoolComponent = (props: { accessedBy: string,children:ReactNode }
                                             <img src={data.logo} className="text-center align-self-center" height={100} alt="" />
                                         </div>
                                         <Card elevation={4} className="border rounded-0 p-1">
-                                            <p className="fw-bold"><SchoolSharp /> {data.name}</p>
-                                            <div className="mb-1"><b>Total Faculty </b> <span
-                                                className="badge bg-primary">{data.facultyList.length}</span>
+                                            <p className="fw-bold"><small><SchoolSharp /> {data.name}</small></p>
+                                            <div className="mb-1"><small>Total Faculty </small> 
+                                            <small className="badge bg-primary">{data.facultyList.length}</small>
                                             </div>
-                                            <div className="mb-1"><PinDrop />{data.location.Location.Location.name}/{data.location.Location.name}/{data.location.name}</div>
-                                            <div><Groups2 />Total Student</div>
+                                            <div className="mb-1"><PinDrop /><small>{data.location.Location.Location.name} || {data.location.Location.name} || {data.location.name}</small></div>
                                             <div className="modal-footer">
-                                                <Tooltip placement="top" title={'create ' + data.name + " Admin"}>
-                                                    <LockPersonRounded className="mx-2 mb-3 bg-primary rounded-2 p-1 text-light" />
-                                                </Tooltip>
                                                 <Tooltip placement="top" title={'View ' + data.name + " Detail"}>
                                                     <ListAlt onClick={() => navigation("/" + props.accessedBy + "/school-detail/" + data.id)} className="mx-2 mb-3 bg-success rounded-2 p-1 text-light" />
                                                 </Tooltip>
@@ -65,8 +62,8 @@ export const SchoolComponent = (props: { accessedBy: string,children:ReactNode }
                         }
                     </div>
                 </div>
-                <Dialog maxWidth='xs' PaperProps={{className:'rounded-0 col-12'}} open={addNewHospital}>
-                    <div className="p-3 sticky-top bg-white">Add new school <Close onClick={()=>setAddNewHospital(false)} className="float-end"/></div>
+                <Dialog maxWidth='xs' PaperProps={{className:'rounded-0 col-12'}} open={addNewSchool}>
+                    <div className="p-3 sticky-top bg-white">Add new school <Close onClick={()=>setAddNewSchool(false)} className="float-end"/></div>
                     <SchoolFormModal/>
                 </Dialog>
             </div>}
