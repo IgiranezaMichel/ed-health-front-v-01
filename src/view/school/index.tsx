96ba88769ea8f87919c3eae3983e269ac1ff0fab
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-29 15:50:21 +0200

    change col to col-sm-6

--- src/view/school/index.tsx
+++ src/view/school/index.tsx
@@ -1,95 +1,105 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Person, PersonAddAlt, Phone, RemoveRedEye, School, SchoolOutlined, Visibility, } from "@mui/icons-material";
-import { Card, Divider } from "@mui/material";
+import { LocationOn, PersonAddAlt, Phone, RemoveRedEye, School, SchoolOutlined, Visibility, Wc, } from "@mui/icons-material";
+import { Avatar, Card, Divider, Tooltip } from "@mui/material";
 import { SchoolMenu } from "../../MenuBarItems/SchoolMenu";
 import { Navigation } from "../../components/default/Navigation";
 import { PIE_CHART_DEFAULT } from "../../components/default/PIECHART";
 import { useShowStudentFromSameSchoolStatusStatistics } from "../../controller/viewHooks/Student/ShowStudentFromSameSchoolStatusStatistics";
 export const SchoolDashBoard = () => {
-  const school=JSON.parse(String(localStorage.getItem("school")));
-  const {isLoadingStudentStatusStatistics,studentsStatusStatistics}=useShowStudentFromSameSchoolStatusStatistics(Number(school.id));
-const user=JSON.parse(String(localStorage.getItem("userData")));
+  const school = JSON.parse(String(localStorage.getItem("school")));
+  const { isLoadingStudentStatusStatistics, studentsStatusStatistics } = useShowStudentFromSameSchoolStatusStatistics(Number(school.id));
+  const user = JSON.parse(String(localStorage.getItem("userData")));
   return (
     <div>
       <Navigation items={SchoolMenu}>
         <>
-        <Card className="row m-auto" elevation={8}>
-          <Card className="col-sm-3 d-flex align-content-center align-items-center justify-content-center">
-              <img src={user.profilePicture} className="card-img" />
-          </Card>
-          <Card className="col-sm-3 d-flex align-content-center align-items-center justify-content-center">
-              <div>
-              <div className="mb-2"><Person/> {user.name}</div>
-              <div className="mb-2"><Person/> {user.gender}</div>
-              <div className="mb-2"><Phone/> {user.phoneNumber}</div>
-              </div>
-          </Card>
-          <Card className="col-sm-3">
-          <img src={school.logo} className="card-img" />
-          </Card>
-          <Card className="col-sm-3 d-flex align-content-center align-items-center justify-content-center">
-              <div>
-              <div className="mb-2"><School/> {school.name}</div>
-              <div className="mb-2"><Person/> {school.location.Location.Location.name}/{school.location.Location.name}/{school.location.name}</div>
-              <div className="mb-2">Total Faculty <a href="#" className="badge bg-primary">{school.facultyList.length}</a>
-              </div>
-              </div>
-          </Card>
-        </Card>
-        <Divider/>      
-        {!isLoadingStudentStatusStatistics&&studentsStatusStatistics.length!=0&&
-        <div className="row col-12 m-auto  mt-2" >
-          <Card className="col-sm-6 border border-3">
-            <div className="fw-bold fs-2 mb-2">Student Status</div>
-          <PIE_CHART_DEFAULT centerLabel="Student" items={studentsStatusStatistics}/>
-          <a href="/school/student"><RemoveRedEye className="float-end"/></a>
-          </Card>
-          <Card className="col-sm-6 border-0 d-flex align-content-center align-items-center justify-content-center">
-            <main className="col-sm-12">
-            <section className="col-sm-12 mb-2">
-              <Card className="border" elevation={3}>
-                <div className="p-2">
-                  <Person className="float-end fs-1" style={{}} />
-                  <span className="fw-bolder">Total Student </span>
-                  <div>
-                    Total Student <span className="badge bg-primary">12</span>
-                  </div>
-                </div>
-                <div className="modal-footer border-top p-2 border-2">
-                  <a className="mx-2">
-                    <Visibility />
-                  </a>
-                  <a className="mx-3">
-                    <PersonAddAlt />
-                  </a>
+          <section className="row m-auto g-3">
+            <section className="col-sm-6 align-content-center">
+              <Card className="p-2 rounded-0" elevation={4}>
+                <Tooltip title={<div><img src={user.profilePicture} height={'100vh'} alt="" /></div>}>
+                  <section className="d-flex">
+                    <Avatar className="border bg-black" src={user.profilePicture} />
+                    <div className="card mx-2  d-flex justify-content-center border-0">{user.name}</div>
+                  </section>
+                </Tooltip>
+                <div className="d-sm-flex mt-2">
+                  <div className="mb-2 mx-1"><Wc className="border border-4 border-black rounded-circle " /> {user.gender}</div>
+                  <div className="mb-2 mx-1"><Phone className="border border-4 border-black rounded-circle " /> {user.phoneNumber}</div>
                 </div>
               </Card>
             </section>
-            <section className="col-sm-12">
-              <Card className="border" elevation={3}>
-                <div className="p-2">
-                  <SchoolOutlined className="float-end fs-1" style={{}} />
-                  <span className="fw-bolder">Graduate Students </span>
-                  <div>
-                    Total Graduates <span className="badge bg-primary">12</span>
-                  </div>
+            <section className="col-sm-6">
+              <Card elevation={4} className="rounded-0 d-flex p-3">
+                <div className="col-sm-1 card border-0 d-flex justify-content-center">
+                  <img src={school.logo} className="card-img" />
                 </div>
-                <div className="modal-footer border-top p-2 border-2">
-                  <a className="mx-2">
-                    <Visibility />
-                  </a>
-                  <a className="mx-3">
-                    <PersonAddAlt />
-                  </a>
+                <div className="col-sm-12 card d-flex border-0 mx-4 align-content-center justify-content-center">
+                  <div>
+                    <div className="mb-2"><School /> {school.name}</div>
+                    <div className="mb-2"><LocationOn /> {school.location.Location.Location.name} || {school.location.Location.name} || {school.location.name}</div>
+                  </div>
                 </div>
               </Card>
             </section>
-            </main>
-          </Card>
-        </div>
-        }
+          </section>
+          <Divider />
+          {!isLoadingStudentStatusStatistics && studentsStatusStatistics.length != 0 &&
+            <div className="row col-12 m-auto g-3  mt-2" >
+              <section className="col-sm-6  border-3">
+              <Card elevation={4} className="rounded-0">
+                <div className="fw-bold p-2 mb-2">Student Status</div>
+                <PIE_CHART_DEFAULT centerLabel="Student" items={studentsStatusStatistics} />
+                <a href="/school/student"><RemoveRedEye className="float-end" /></a>
+              </Card>
+              </section>
+              <section className="col-sm-6 border-0 d-flex align-content-center align-items-center justify-content-center">
+              <Card className="col-12 border-0">
+                <main className="col-sm-12">
+                  <section className="col-sm-12 mb-4">
+                    <Card className="border" elevation={3}>
+                      <div className="p-2">
+                        <School className="float-end fs-1" />
+                        <span className="fw-bolder">Total Faculties </span>
+                        <div>
+                          Total Faculty <span className="badge bg-primary">{school.facultyList.length}</span>
+                        </div>
+                      </div>
+                      <div className="modal-footer border-top p-2 border-2">
+                        <a className="mx-2">
+                          <Visibility />
+                        </a>
+                        <a className="mx-3">
+                          <PersonAddAlt />
+                        </a>
+                      </div>
+                    </Card>
+                  </section>
+                  <section className="col-sm-12">
+                    <Card className="border" elevation={3}>
+                      <div className="p-2">
+                        <SchoolOutlined className="float-end fs-1" style={{}} />
+                        <span className="fw-bolder">Graduate Students </span>
+                        <div>
+                          Total Graduates <span className="badge bg-primary">12</span>
+                        </div>
+                      </div>
+                      <div className="modal-footer border-top p-2 border-2">
+                        <a className="mx-2">
+                          <Visibility />
+                        </a>
+                        <a className="mx-3">
+                          <PersonAddAlt />
+                        </a>
+                      </div>
+                    </Card>
+                  </section>
+                </main>
+              </Card>
+              </section>
+            </div>
+          }
         </>
-       
+
       </Navigation>
     </div>
   );
