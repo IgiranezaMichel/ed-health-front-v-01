dbd33cfe9690f0d5ad27a6f160a97223fae79e36
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-30 16:47:46 +0200

    NativeSelect

--- src/view/ncnm/Training/components/AppendingTrainingList.tsx
+++ src/view/ncnm/Training/components/AppendingTrainingList.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { CalendarToday, ListAltSharp, LocalHospital, LocationOn, Sort } from "@mui/icons-material";
-import { Card, Pagination, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { CalendarToday, ListAltSharp,  LocationOn, Sort } from "@mui/icons-material";
+import { Avatar, Card, NativeSelect, Pagination, Table, TableBody, TableCell, TableHead, TableRow, Tooltip } from "@mui/material";
 import { FC, useState } from "react";
 import { useAppendingTraining } from "../../../../controller/viewHooks/training/useAppendingTraining";
 import { PaginationInput } from "../../../../typeDefs/PaginationInput";
@@ -19,17 +19,17 @@ export const AppendingTrainingList: FC<trainingControl> = (props) => {
     return (
         <>
            {appendingTrainingDetail.totalPages!=0&&<div className="mt-4">  Page {appendingTrainingDetail.pageNumber + 1} out of {appendingTrainingDetail.totalPages}  <span>
-                <select onChange={(e) => setPage({ ...page, pageSize: Number(e.target.value) })} className="p-1 mx-2">
+                <NativeSelect onChange={(e) => setPage({ ...page, pageSize: Number(e.target.value) })} className="p-1 mx-2">
                     <option value={8}>8</option>
                     <option value={16}>16</option>
                     <option value="24">24</option>
-                </select>
+                </NativeSelect>
             </span>
-                <span className="float-end"> Sort by<select onChange={e => setPage({ ...page, sort: e.target.value })} className="custom-select p-1" name="" id="">
+                <span className="float-end"> Sort by <Sort /> <NativeSelect onChange={e => setPage({ ...page, sort: e.target.value })} className="custom-select p-1" name="" id="">
                     <option selected={page.sort == 'description' ? true : false} value="description">Description</option>
                     <option selected={page.sort == 'deadline' ? true : false} value="deadline">Deadline</option>
                     <option selected={page.sort == 'location' ? true : false} value="location">Location</option>
-                </select><Sort /></span>
+                </NativeSelect></span>
                 <Pagination count={appendingTrainingDetail.totalPages} onChange={handleChange} page={appendingTrainingDetail.pageNumber + 1} />
             </div>}
             <Card className="overflow-auto mt-5">
@@ -39,7 +39,6 @@ export const AppendingTrainingList: FC<trainingControl> = (props) => {
                         <TableCell className="fw-bold">#</TableCell>
                         <TableCell className="fw-bold">Hospital</TableCell>
                         <TableCell className="fw-bold">Title</TableCell>
-                        <TableCell className="fw-bold">Published_On</TableCell>
                         <TableCell className="fw-bold">Deadline</TableCell>
                         <TableCell className="fw-bold">Action</TableCell>
                     </TableRow>
@@ -51,16 +50,20 @@ export const AppendingTrainingList: FC<trainingControl> = (props) => {
                                 <TableRow>
                                     <TableCell>{index + 1}</TableCell>
                                     <TableCell className="col-sm-2">
+                                        <div className="d-flex">
+                                            <Tooltip title={<div><img className="card-img p-1" height={'100vh'} src={data.hospital.logo} /></div>}>
+                                            <Avatar className="card-img p-1" src={data.hospital.logo} />
+                                            </Tooltip>
+                                            <div className="card d-flex justify-content-center border-0">{data.hospital.name}</div>
+                                        </div>
                                         <div>
-                                            <img className="card-img" src={data.hospital.logo} alt="" />
-                                            <div><LocalHospital />{data.hospital.name}</div>
+                                        {String(data.timeStamp).split('T')[0]}
                                         </div>
                                     </TableCell>
                                     <TableCell>{data.title}</TableCell>
-                                    <TableCell><CalendarToday />{String(data.timeStamp).split('T')[0]}</TableCell>
                                     <TableCell>
-                                        <div> <CalendarToday />{String(data.deadline).split('T')[0]}</div>
-                                        <div><LocationOn />{data.location.Location.Location.name}/{data.location.Location.name}/{data.location.name}</div>
+                                        <div className="mb-2"> <CalendarToday className="border rounded-circle border-black p-1"/> {String(data.deadline).split('T')[0]}</div>
+                                        <div className="m"><LocationOn className="border rounded-circle border-black p-1"/> {data.location.Location.Location.name} || {data.location.Location.name} || {data.location.name}</div>
                                     </TableCell>
                                     <TableCell><ListAltSharp onClick={() => navigate("/ncnm/training-detail/" + data.id)} /></TableCell>
                                 </TableRow>
