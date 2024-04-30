2a5f7d5360d7c8cab5451fe33f20bb3295c2bfe0
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-04-30 16:47:18 +0200

    useGetStudentAppliedForTraining

--- src/view/hospital/Training/TrainingDetail.tsx
+++ src/view/hospital/Training/TrainingDetail.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useMutation } from "@apollo/client";
 import { BorderColor, KeyboardArrowDown, KeyboardArrowUp, People, PersonPin, PhoneInTalkOutlined, PlaylistAdd, RemoveRedEye, Save, TitleSharp } from "@mui/icons-material";
-import { Box, Button, Card, CircularProgress, Skeleton, Tab, TextField } from "@mui/material";
+import { Avatar, Box, Button, Card, CircularProgress, NativeSelect, Skeleton, Tab, TextField, Tooltip } from "@mui/material";
 import { ChangeEvent, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { HospitalMenu } from "../../../MenuBarItems/HospitalMenu";
@@ -12,6 +12,7 @@ import { TrainerInput } from "../../../typeDefs/TrainerInput";
 import { CalendarIcon } from "@mui/x-date-pickers";
 import { Modal } from "../../../components/default/Modal";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { useGetStudentAppliedForTraining } from "../../../controller/viewHooks/TrainingApplication/trainingApplication";
 export const HospitalTrainingDetail = () => {
     const { id } = useParams();
     const [isSaving, setIsSaving] = useState(false);
@@ -56,6 +57,7 @@ export const HospitalTrainingDetail = () => {
         }
     }
     const { isFindingTraining, applicantList, trainerList, trainingDetail, refreshTrainingDetail } = useFindTrainingById(Number(id));
+    const { studentList, isLoading } = useGetStudentAppliedForTraining(Number(id));
     const navigate = useNavigate()
     const addTrainerModal = <Modal
         actionBtn={
@@ -66,16 +68,16 @@ export const HospitalTrainingDetail = () => {
         }
         id="add-school-admin" title="Add School Admin">
         <div className="p-3">
-            <select onChange={(e) => setTrainer({ ...trainer, trainerTitle: e.target.value })} className="mb-3 p-3 w-25" name="" id="">
+            <NativeSelect onChange={(e) => setTrainer({ ...trainer, trainerTitle: e.target.value })} className="mb-3 mt-3 w-25">
                 <option value="Sir.">Sir</option>
                 <option value="Madam.">Madam</option>
                 <option value="Mr.">Mister</option>
                 <option value="Mrs.">Mistress</option>
                 <option value="Dr.">Doctor</option>
                 <option value="Pr.">Professor</option>
-            </select>
-            <TextField type="text" value={trainer.name} onChange={(e) => setTrainer({ ...trainer, name: e.target.value })} className="mb-3  w-75" label="Trainer Name" />
-            <TextField type="text" value={trainer.phoneNumber} onChange={(e) => setTrainer({ ...trainer, phoneNumber: e.target.value })} className="mb-3 form-control" label="Phone Number" />
+            </NativeSelect>
+            <TextField type="text" value={trainer.name} onChange={(e) => setTrainer({ ...trainer, name: e.target.value })} className="mb-3  w-75" label="Trainer Name" variant="standard" />
+            <TextField type="text" value={trainer.phoneNumber} onChange={(e) => setTrainer({ ...trainer, phoneNumber: e.target.value })} className="mb-3 form-control" label="Phone Number" variant="standard" />
             <label htmlFor="">Profile picture</label>
             <input type="file" onChange={handleProfilePic} className="form-control mb-2" />
             <label htmlFor="">Upload Signature</label>
@@ -100,7 +102,12 @@ export const HospitalTrainingDetail = () => {
                         </div>
                         <div className="col-md-6">
                             <span className="d-block mb-1"><CalendarIcon /> Deadline: <b>{String(trainingDetail.deadline).split('T')[0]}</b></span>
-                            <span className="d-block">Post Date: <b>{trainingDetail.timeStamp}</b></span>
+                            <span className="d-block">
+                                <span className="fw-bold">
+                                    <People />Total Applicant <span className="badge bg-info text-dark">{applicantList.length}</span>
+                                    <RemoveRedEye onClick={() => navigate('/hospital/training-applicant-detail/' + id)} className="mx-2" />
+                                </span>
+                            </span>
                         </div>
                     </Card>
                     <Card className="mt-3 rounded-0" elevation={4}>
@@ -108,8 +115,22 @@ export const HospitalTrainingDetail = () => {
                             <Box sx={{ border: 'none', borderColor: 'divider' }}>
                                 <TabList onChange={handleChange}>
                                     <Tab label={<div><PersonPin /> Requirement</div>} value="1" />
-                                    <Tab label={<div><People /> Trainers</div>} value="2" />
-                                    <Tab label={<div><People /> Applicant List</div>} value="3" />
+                                    <Tab label={<div>
+                                        <span className=" btn border-0 position-relative">
+                                            <People /> Trainers
+                                            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                                                {trainerList.length}
+                                            </span>
+                                        </span>
+                                    </div>} value="2" />
+                                    <Tab label={<div>
+                                        <span className=" btn border-0 position-relative">
+                                            <People /> Applicant list
+                                            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                                                {!isLoading && studentList != undefined && <>{studentList.length}</>}
+                                            </span>
+                                        </span>
+                                    </div>} value="3" />
                                 </TabList>
                             </Box>
                             <TabPanel value="1" className="col-12">
@@ -123,23 +144,38 @@ export const HospitalTrainingDetail = () => {
                                 <div>
                                     <span className="fw-bold fs-5 d-block">Trainer</span>
                                     <div className="modal-footer">
-                                        <PlaylistAdd data-bs-toggle="modal" data-bs-target="#add-school-admin" className="bg-primary p-1 fs-1 text-light rounded-circle" />
+                                        <button className="bg-primary p-1 text-light btn">
+                                            <PlaylistAdd data-bs-toggle="modal" data-bs-target="#add-school-admin" />
+                                        </button>
                                     </div>
                                     <div className="col-12 row m-auto g-1">
-                                        {show == 'trainerList' && trainerList.map((data: any) => {
-                                            return <section className="col-sm-4 p-0">
-                                                <Card className="p-0" elevation={4}>
-                                                    <img src={data.profilePicture} style={{ height: '150px', width: '100%', objectFit: 'contain' }} alt="" />
-                                                    <div className="card-body">
-                                                        <div className="card-title px-1">
-                                                            <b>{data.trainerTitle}</b> {data.name}
-                                                            <div className="mt-2 ">
-                                                                <PhoneInTalkOutlined />{data.phoneNumber}
-                                                            </div>
+                                        {trainerList.map((data: any) => {
+                                            return <Card className="p-3 row col-12 m-auto mt-3" elevation={4}>
+                                                <section className="col-md-6">
+                                                    <section className='d-flex'>
+                                                        <Tooltip arrow title={<img src={data.profilePicture} height={'100vh'} />}>
+                                                            <Avatar className="bg-black card-img" src={data.profilePicture} />
+                                                        </Tooltip>
+                                                        <div className="card mx-2 d-flex justify-content-center border-0">
+                                                            <div><b>{data.trainerTitle}</b> {data.name}</div>
                                                         </div>
+                                                    </section>
+
+                                                </section>
+                                                <section className="col-md-3">
+                                                    <div className="mt-2 ">
+                                                        <PhoneInTalkOutlined />{data.phoneNumber}
                                                     </div>
-                                                </Card>
-                                            </section>
+                                                </section>
+                                                <section className="col-md-3">
+                                                    <Tooltip title={<div>
+                                                        <div><b>{data.trainerTitle}</b> {data.name}</div>
+                                                        <div>Signature</div>
+                                                    </div>} className="float-end">
+                                                        <div><img src={data.signature} height={30} /><br /></div>
+                                                    </Tooltip>
+                                                </section>
+                                            </Card>
                                         })}
                                     </div>
                                     <div className="text-center">
@@ -150,15 +186,22 @@ export const HospitalTrainingDetail = () => {
                                 </div>
 
                             </TabPanel>
-                            <TabPanel value="3">g</TabPanel>
+                            <TabPanel value="3">
+                                {!isLoading && studentList != undefined && studentList.length != 0 &&
+                                    <div >
+                                        display something
+                                    </div>
+
+                                }
+                                {!isLoading && studentList != undefined && studentList.length == 0 &&
+                                    <div className="fw-bold text-center p-3">
+                                        -- No Applicant data found --
+                                    </div>
+
+                                }
+                            </TabPanel>
                         </TabContext>
                     </Card>
-                    <div className="my-3">
-                        <span className="bg-primary my-1 text-white fw-bold p-2 rounded-0 border">
-                            Total Applicant <span className="badge bg-info text-dark">{applicantList.length}</span>
-                            <RemoveRedEye onClick={() => navigate('/hospital/training-applicant-detail/' + id)} className="mx-2" />
-                        </span>
-                    </div>
                 </>
             }
             {addTrainerModal}
